1c29d686ec1f3baa465abc54e5c837930a0ae1fe
Kevin Garner <kevin.garner@ssigroup.com>
2023-06-28 17:23:14 -0500

    Added localization data for English.
    Added test settings initialization. Has type errors until I can fix later.
    
    Initialized settings in main.ts on ready.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { FoundryAdapter } from './foundry/foundry-adapter';
 import { Tidy5eSheetKgar } from './sheets/character';
 import './scss/core.scss';
+import { initSettings } from './settings';
 
 FoundryAdapter.registerCharacterSheet(Tidy5eSheetKgar);
+
+FoundryAdapter.onReady(() => {
+  initSettings();
+});
