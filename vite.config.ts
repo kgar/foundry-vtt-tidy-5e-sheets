af013abe3d109bdbdecadd248209842b09edf10b
Kevin Garner <kevin.garner@ssigroup.com>
2023-06-28 16:04:14 -0500

    Fixed headache issue with VS Code importing stuff as "src/path/to/content" and not being recognized.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { resolve } from 'path';
+import path, { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      src: path.resolve('./src')
+    }
+  },
   build: {
     lib: {
       entry: 'src/main.ts',
