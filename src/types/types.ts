62b45e0695c086551c9e7ca52f7750ef88f56b38
Kevin Garner <kevinleegarner@gmail.com>
2023-06-29 23:45:33 -0500

    Added needed sheet functions

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,7 @@
 export type SheetFunctions = {
   activateListeners: () => void;
   submit: () => void;
+  render: (force: boolean) => void;
+  onShortRest: (event: Event) => void;
+  onLongRest: (event: Event) => void;
 };
