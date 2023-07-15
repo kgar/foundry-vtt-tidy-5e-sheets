33112ed1d262ce9fbb747210aa4278fff53ac5f6
Kevin Garner <kevinleegarner@gmail.com>
2023-07-15 18:15:40 -0500

    Created ItemControls component to house item controls and provide standard styles.

---
+++ src/components/items/ItemControls.svelte
@@ -0,0 +1,19 @@
+<div class="tidy5e-item-controls">
+  <slot />
+</div>
+
+<style lang="scss">
+  .tidy5e-item-controls {
+    align-self: stretch;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    min-width: 5.3125rem;
+    font-size: 0.75rem;
+    gap: 0.125rem;
+
+    :global(> *) {
+      flex: 1;
+    }
+  }
+</style>
