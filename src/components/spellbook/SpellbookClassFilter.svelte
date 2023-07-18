c7dcd76abdf31ac51b695d1b975cd6b5868dd060
Kevin Garner <kevin.garner@ssigroup.com>
2023-07-18 11:44:10 -0500

    Added spellbook class filter component.

---
+++ src/components/spellbook/SpellbookClassFilter.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  import { CONSTANTS } from 'src/constants';
+  import {
+    FoundryAdapter,
+    type CharacterSheetContext,
+  } from 'src/foundry/foundry-adapter';
+  import { SettingsProvider } from 'src/settings/settings';
+  import type { DropdownOption } from 'src/types/types';
+
+  export let context: CharacterSheetContext;
+
+  const allClasses: DropdownOption[] = Object.entries(
+    CONSTANTS.DND5E_CLASSES
+  ).map((x) => ({
+    value: x[0],
+    text: x[1],
+  }));
+
+  const additionalClassText =
+    SettingsProvider.settings.spellClassFilterAdditionalClasses.get() ?? '';
+
+  if (additionalClassText?.trim() !== '') {
+    const additionalClasses = additionalClassText
+      .split(',')
+      .reduce((arr: DropdownOption[], x: string) => {
+        const pieces = x.split('|');
+        if (pieces.length !== 2) {
+          return arr;
+        }
+        arr.push({
+          value: pieces[0],
+          text: pieces[1],
+        });
+        return arr;
+      }, []);
+
+    allClasses.push(...additionalClasses);
+  }
+
+  allClasses.sort((a, b) => a.text.localeCompare(b.text));
+
+  allClasses.unshift({ text: 'DND5E.Spellbook', value: '' });
+
+  const localize = FoundryAdapter.localize;
+
+  const selectedClassFilter =
+    FoundryAdapter.tryGetFlag(context.actor, 'classFilter') ?? '';
+</script>
+
+<select class="class-filter" name="flags.{CONSTANTS.MODULE_ID}.classFilter">
+  {#each allClasses as option}
+    <option
+      value={option.value}
+      selected={option.value === selectedClassFilter ?? undefined}
+      >{localize(option.text)}</option
+    >
+  {/each}
+</select>
+
+<style lang="scss">
+  .class-filter {
+    text-overflow: ellipsis;
+  }
+</style>
