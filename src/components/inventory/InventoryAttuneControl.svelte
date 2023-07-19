23e735966a5624fc6115e7341c85beff3c0db198
Kevin Garner <kevin.garner@ssigroup.com>
2023-07-19 17:16:10 -0500

    Added InventoryAttuneControl and InventoryEquipControl.

---
+++ src/components/inventory/InventoryAttuneControl.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+  import ItemControl from '../items/ItemControl.svelte';
+  import type { Item5e } from 'src/types/item';
+
+  export let item: Item5e;
+  export let ctx: any;
+
+  const isAttuned = item.system.attunement === 2;
+
+  function toggleAttuned() {
+    const actor = item.actor;
+    if (!actor) {
+      return;
+    }
+
+    const currentAttunementCount = actor.system.attributes.attunement.value;
+
+    if (item.system.attunement == 2) {
+      item.update({ 'system.attunement': 1 });
+    } else {
+      if (currentAttunementCount >= actor.system.attributes.attunement.max) {
+        ui.notifications.warn(
+          `${FoundryAdapter.localize('TIDY5E.AttunementWarning', {
+            number: currentAttunementCount,
+          })}`
+        );
+      } else {
+        item.update({ 'system.attunement': 2 });
+      }
+    }
+  }
+</script>
+
+<ItemControl
+  title={ctx.attunement?.title}
+  iconCssClass="fas fa-sun {!isAttuned ? 'not-attuned' : ''}"
+  active={isAttuned}
+  on:click={toggleAttuned}
+/>
