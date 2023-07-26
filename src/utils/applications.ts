4db6faa9b5d2951ee6336de5a9585215e61c17c3
Kevin Garner <kevin.garner@ssigroup.com>
2023-07-26 16:09:37 -0500

    Added util for updating window titles.

---
+++ src/utils/applications.ts
@@ -0,0 +1,16 @@
+export function applyTitleToWindow(title: string, element: HTMLElement) {
+  let windowTitle: HTMLElement | ChildNode | null =
+    element.querySelector('.window-title');
+
+  if (windowTitle?.hasChildNodes()) {
+    windowTitle = windowTitle.childNodes[0];
+  }
+
+  if (!windowTitle) {
+    return;
+  }
+
+  if (windowTitle.textContent !== title) {
+    windowTitle.textContent = title;
+  }
+}
