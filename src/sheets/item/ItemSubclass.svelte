320189c330ea4288162f97470fddded7f5b8321a
Kevin Garner <kevin.garner@ssigroup.com>
2023-08-04 15:29:47 -0500

    Finished scaffolding subclass sheet.

--- src/sheets/item/ItemSubclass.svelte
+++ src/sheets/item/ItemSubclass.svelte
@@ -3,11 +3,15 @@
   import type { Readable } from 'svelte/store';
   import type { ItemSheetContext } from 'src/types/item';
   import type { Tab } from 'src/types/types';
-  import ItemDescriptionWithSidebar from './parts/ItemDescriptionWithSidebar.svelte';
   import ItemSubclassDetails from './parts/ItemSubclassDetails.svelte';
   import { CONSTANTS } from 'src/constants';
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
   import ItemAdvancement from './parts/ItemAdvancement.svelte';
+  import Tabs from 'src/components/tabs/Tabs.svelte';
+  import TabContents from 'src/components/tabs/TabContents.svelte';
+  import ItemDescription from './parts/ItemDescription.svelte';
+  import TextInput from 'src/components/form/TextInput.svelte';
+  import ItemProfilePicture from './parts/ItemProfilePicture.svelte';
 
   let store = getContext<Readable<ItemSheetContext>>('store');
 
@@ -20,8 +24,8 @@
       id: CONSTANTS.TAB_ITEM_DESCRIPTION_ID,
       displayName: 'DND5E.Description',
       content: {
-        component: ItemDescriptionWithSidebar,
-        cssClass: 'flexrow',
+        component: ItemDescription,
+        cssClass: 'flexcol',
       },
     },
     {
@@ -49,3 +53,37 @@
 
   const localize = FoundryAdapter.localize;
 </script>
+
+<header class="sheet-header flexrow gap">
+  <ItemProfilePicture />
+  <div class="header-details flexrow">
+    <h1 class="charname">
+      <TextInput
+        field="name"
+        document={$store.item}
+        value={$store.item.name}
+        placeholder={localize('DND5E.SubclassName')}
+      />
+    </h1>
+
+    <div class="item-subtitle">
+      <h4 class="item-type">{$store.itemType}</h4>
+      <span class="item-status">{$store.itemStatus ?? ''}</span>
+    </div>
+
+    <ul class="summary flexrow">
+      <li>
+        <TextInput
+          field="system.source"
+          document={$store.item}
+          value={$store.system.source}
+          placeholder={localize('DND5E.Source')}
+        />
+      </li>
+    </ul>
+  </div>
+</header>
+<Tabs bind:selectedTabId {tabs} />
+<div class="sheet-body">
+  <TabContents {tabs} {selectedTabId} />
+</div>
