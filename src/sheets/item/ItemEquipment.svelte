e6e7caab0074c059808cc2b7a55cac3b4f2429f3
Kevin Garner <kevin.garner@ssigroup.com>
2023-08-04 14:11:30 -0500

    a

--- src/sheets/item/ItemEquipment.svelte
+++ src/sheets/item/ItemEquipment.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
+  import { getContext } from 'svelte';
+  import type { Readable } from 'svelte/store';
   import type { ItemSheetContext } from 'src/types/item';
   import type { Tab } from 'src/types/types';
   import Tabs from 'src/components/tabs/Tabs.svelte';
-  import type { Readable } from 'svelte/store';
   import ItemDescriptionWithSidebar from './parts/ItemDescriptionWithSidebar.svelte';
   import ItemEquipmentDetails from './parts/ItemEquipmentDetails.svelte';
   import ActiveEffects from '../actor/parts/ActiveEffects.svelte';
   import TabContents from 'src/components/tabs/TabContents.svelte';
   import { CONSTANTS } from 'src/constants';
-  import { getContext } from 'svelte';
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
   import ItemProfilePicture from './parts/ItemProfilePicture.svelte';
   import TextInput from 'src/components/form/TextInput.svelte';
