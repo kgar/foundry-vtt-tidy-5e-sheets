c744cdbcc0eb81fe260714759748858512edc976
Kevin Garner <kevinleegarner@gmail.com>
2023-08-12 20:25:46 -0500

    Began work on NPC abilities main panel.

--- src/sheets/npc/NpcAbilitiesTab.svelte
+++ src/sheets/npc/NpcAbilitiesTab.svelte
@@ -6,8 +6,44 @@
   import type { Readable } from 'svelte/store';
   import type { NpcSheetContext } from 'src/types/types';
   import Currency from '../actor/Currency.svelte';
+  import ItemTableHeaderRow from 'src/components/items/ItemTableHeaderRow.svelte';
+  import ItemTable from 'src/components/items/ItemTable.svelte';
+  import ItemTableColumn from 'src/components/items/ItemTableColumn.svelte';
+  import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+  import ItemTableRow from 'src/components/items/ItemTableRow.svelte';
+  import ItemTableCell from 'src/components/items/ItemTableCell.svelte';
+  import { CONSTANTS } from 'src/constants';
+  import ItemUseButton from 'src/components/items/ItemUseButton.svelte';
+  import ItemName from 'src/components/items/ItemName.svelte';
 
   let store = getContext<Readable<NpcSheetContext>>('store');
+
+  let attacksSection: any,
+    actionsSection: any,
+    featuresSection: any,
+    inventorySection: any;
+
+  $: {
+    for (let section of $store.features) {
+      switch (section.dataset?.type) {
+        case 'weapon':
+          attacksSection = section;
+          break;
+        case 'feat':
+          if (section.dataset['activation.type'] === 'action') {
+            actionsSection = section;
+          } else {
+            featuresSection = section;
+          }
+          break;
+        case 'loot':
+          inventorySection = section;
+          break;
+      }
+    }
+  }
+
+  const localize = FoundryAdapter.localize;
 </script>
 
 <section class="npc-abilities-content">
@@ -20,7 +56,60 @@
       toggleable={!SettingsProvider.settings.traitsAlwaysShownNpc.get()}
     />
   </div>
-  <div class="main-panel">Favorites</div>
+  <div class="main-panel">
+    <!-- Attacks -->
+    <ItemTable>
+      <ItemTableHeaderRow>
+        <ItemTableColumn primary={true}>
+          {localize(attacksSection.label)}
+        </ItemTableColumn>
+        <ItemTableColumn baseWidth="3.125rem">
+          {localize('DND5E.Uses')}
+        </ItemTableColumn>
+        <ItemTableColumn baseWidth="7.5rem">
+          {localize('DND5E.Usage')}
+        </ItemTableColumn>
+        <ItemTableColumn baseWidth="7.5rem" />
+      </ItemTableHeaderRow>
+      {#each attacksSection.items as item}
+      {@const ctx = $store.itemContext}
+        <ItemTableRow
+          let:toggleSummary
+          on:mousedown={(event) =>
+            FoundryAdapter.editOnMiddleClick(event.detail, item)}
+          contextMenu={{
+            type: CONSTANTS.CONTEXT_MENU_TYPE_ITEMS,
+            id: item.id,
+          }}
+        >
+          <ItemTableCell primary={true}>
+            <ItemUseButton {item} />
+            <ItemName
+              on:click={(event) => toggleSummary(event.detail, $store.actor)}
+              hasChildren={false}
+            >
+              {item.name}
+            </ItemName>
+          </ItemTableCell>
+          {#if item.hasUses}
+            <ItemTableCell baseWidth="3.125rem">
+              <!-- Uses, if any -->
+            </ItemTableCell>
+          {/if}
+          <ItemTableCell baseWidth="7.5rem">
+            <!-- Usage -->
+          </ItemTableCell>
+          <ItemTableCell baseWidth="7.5rem">
+            <!-- Controles -->
+          </ItemTableCell>
+        </ItemTableRow>
+      {/each}
+    </ItemTable>
+    <!-- Actions -->
+    <!-- Features -->
+    <!-- Inventory -->
+    <!-- To Do: Handle others? -->
+  </div>
 </section>
 <footer>
   <Currency actor={$store.actor} />
