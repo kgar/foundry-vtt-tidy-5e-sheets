247588b699f2ed185edcea69e8963c94c3623505
Kevin Garner <kevin.garner@ssigroup.com>
2023-08-16 13:06:58 -0500

    Converted spell slot uses, attribute block, and initiative block to use the input components.
    Extracted Block Score and Block Title components for sharing across different block types and eliminated the style from global actor styles.

--- src/components/spellbook/SpellbookFooter.svelte
+++ src/components/spellbook/SpellbookFooter.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { CONSTANTS } from 'src/constants';
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
-  import { submitText } from 'src/sheets/form';
   import type { ActorSheetContext } from 'src/types/types';
   import { getContext } from 'svelte';
   import type { Readable } from 'svelte/store';
