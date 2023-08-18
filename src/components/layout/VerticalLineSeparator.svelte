1c70b83116661f634e5f4f8789765c5aeb5477bb
Kevin Garner <kevinleegarner@gmail.com>
2023-08-17 23:18:46 -0500

    Added vertical line separator component.

---
+++ src/components/layout/VerticalLineSeparator.svelte
@@ -0,0 +1,9 @@
+<div class="vertical-line-separator" aria-hidden="true" role="presentation" />
+
+<style lang="scss">
+  .vertical-line-separator {
+    background: var(--t5e-faint-color);
+    width: 0.0625rem;
+    flex: 0 0 auto;
+  }
+</style>
