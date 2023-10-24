7ffa5898b4d87de487b84536c1be5e2d543f0e86
Kevin Garner <kevinleegarner@gmail.com>
2023-10-23 22:32:37 -0500

    Reverted vite config proxy server settings for static files and updated prepareDistForDev function to include an empty main and style file.

--- prepare-dist-for-dev.js
+++ prepare-dist-for-dev.js
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import path from 'path';
 
 /**
  * When doing dev for Tidy 5e Sheets, the style CSS file is ignored, and the main JS file is proxied in by the vite proxy server.
@@ -24,6 +25,14 @@ function prepareDistForDev() {
 
   // Copy all of the public folder contents to dist
   fs.cpSync(publicFolder, distFolder, { recursive: true });
+
+  // Put empty main.js and style.css files in place
+  const message =
+    'Hello, Tidy 5e dev 👋. This file exists so that foundry is happy and will consider the module eligible to load up. The vite server proxy will do the rest.';
+
+  fs.writeFileSync(path.join(distFolder, 'main.js'), `// ${message}`);
+
+  fs.writeFileSync(path.join(distFolder, 'style.css'), `/* ${message} */`);
 }
 
 prepareDistForDev();
