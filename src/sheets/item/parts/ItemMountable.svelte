525690d2c4b170e70f8f52d17d084f8cc284e2b0
Kevin Garner <kevin.garner@ssigroup.com>
2023-10-30 14:07:18 -0500

    Implemented ItemMountable.

--- src/sheets/item/parts/ItemMountable.svelte
+++ src/sheets/item/parts/ItemMountable.svelte
@@ -1,1 +1,55 @@
-<div>TODO: Implement ItemMountable.svelte</div>
+<script lang="ts">
+  import NumberInput from 'src/components/form/NumberInput.svelte';
+  import TextInput from 'src/components/form/TextInput.svelte';
+  import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+  import type { ItemSheetContext } from 'src/types/item';
+  import { getContext } from 'svelte';
+  import type { Readable } from 'svelte/store';
+
+  let context = getContext<Readable<ItemSheetContext>>('context');
+  let appId = getContext<Readable<string>>('appId');
+
+  const localize = FoundryAdapter.localize;
+</script>
+
+<div class="form-group">
+  <label for="{$appId}-system.hp.value">{localize('DND5E.HitPoints')}</label>
+  <div class="form-fields">
+    <NumberInput
+      id="{$appId}-system.hp.value"
+      document={$context.item}
+      field="system.hp.value"
+      value={$context.item.system.hp.value}
+      placeholder="0"
+    />
+    <span class="sep">&sol;</span>
+    <NumberInput
+      id="{$appId}-system.hp.max"
+      document={$context.item}
+      field="system.hp.max"
+      value={$context.item.system.hp.max}
+      placeholder="0"
+    />
+    <NumberInput
+      document={$context.item}
+      field="system.hp.dt"
+      id="{$appId}-system.hp.dt"
+      placeholder={localize('DND5E.Threshold')}
+      value={$context.item.system.hp.dt}
+    />
+  </div>
+</div>
+
+<div class="form-group">
+  <label for="{$appId}-system.hp.conditions"
+    >{localize('DND5E.HealthConditions')}</label
+  >
+  <div class="form-fields">
+    <TextInput
+      id="{$appId}-system.hp.conditions"
+      document={$context.item}
+      field="system.hp.conditions"
+      value={$context.item.system.hp.conditions}
+    />
+  </div>
+</div>
