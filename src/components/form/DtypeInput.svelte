aada13cdaa99efbd7de7630748cbcdf89ed28e9b
Kevin Garner <kevinleegarner@gmail.com>
2023-10-30 22:48:08 -0500

    Added DtypeInput component to handle cases where a dynamic editable table cell is provided.

---
+++ src/components/form/DtypeInput.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Checkbox from './Checkbox.svelte';
+  import NumberInput from './NumberInput.svelte';
+  import TextInput from './TextInput.svelte';
+
+  export let document: any;
+  export let field: string;
+  export let value: any;
+  export let dtype: string;
+</script>
+
+{#if dtype === 'Number'}
+  <NumberInput {document} {field} {value} />
+{:else if dtype === 'Boolean'}
+  <Checkbox {document} {field} {value} />
+{:else}
+  <TextInput {document} {field} {value} />
+{/if}
