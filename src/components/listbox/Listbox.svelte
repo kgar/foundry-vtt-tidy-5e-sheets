3b87c673a73cbd42cb01358959cd69e3880c2ea4
Kevin Garner <kevinleegarner@gmail.com>
2023-11-20 23:21:36 -0600

    Added basic listbox and selection listbox to available components.

---
+++ src/components/listbox/Listbox.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  export let items: any[];
+  export let labelProp: string;
+  export let valueProp: string;
+  export let selectedItemIndex: number | null = null;
+
+  let idRandomizer = Math.random().toString().substring(2);
+
+  const dispatcher = createEventDispatcher<{
+    select: number;
+    keydown: KeyboardEvent & { currentTarget: HTMLElement };
+  }>();
+
+  let listbox: HTMLElement;
+
+  function handleListboxKeyDown(
+    ev: KeyboardEvent & { currentTarget: HTMLElement }
+  ) {
+    if (items.length) {
+      const currentIndex = selectedItemIndex ?? -1;
+
+      if (ev.key === 'ArrowUp' && !ev.altKey) {
+        selectItemAt(Math.max(0, currentIndex - 1));
+        ev.preventDefault();
+      } else if (ev.key === 'ArrowDown' && !ev.altKey) {
+        selectItemAt(Math.min(items.length - 1, currentIndex + 1));
+        ev.preventDefault();
+      } else if (ev.key === 'Home') {
+        selectItemAt(0);
+        ev.preventDefault();
+      } else if (ev.key === 'End') {
+        selectItemAt(items.length - 1);
+        ev.preventDefault();
+      }
+
+      listbox
+        .querySelector(`#listbox-item-${selectedItemIndex}-${idRandomizer}`)
+        ?.scrollIntoView({ block: 'nearest' });
+    }
+
+    dispatcher('keydown', ev);
+  }
+
+  function selectItemAt(index: number) {
+    selectedItemIndex = index;
+    dispatcher('select', index);
+  }
+</script>
+
+<ul
+  bind:this={listbox}
+  role="listbox"
+  class="listbox {$$props.class ?? ''}"
+  aria-activedescendant={selectedItemIndex !== null
+    ? `listbox-item-${selectedItemIndex}-${idRandomizer}`
+    : null}
+  tabindex="0"
+  on:keydown={handleListboxKeyDown}
+>
+  {#each items as item, i (item[valueProp])}
+    <li
+      id="listbox-item-{i}-{idRandomizer}"
+      role="option"
+      aria-selected={selectedItemIndex === i}
+      class:focused={selectedItemIndex === i}
+      on:click={() => selectItemAt(i)}
+      on:keydown={(ev) => handleListboxKeyDown(ev)}
+    >
+      {item[labelProp]}
+    </li>
+  {/each}
+</ul>
+
+<style>
+  ul.listbox {
+    margin: 0;
+    padding: 0;
+    border: 1px solid gray;
+    overflow-y: auto;
+    max-height: inherit;
+    height: 100%;
+  }
+  .listbox li {
+    list-style-type: none;
+    padding: 0.5rem;
+  }
+  .listbox [role='option'].focused {
+    background-color: #ffc0cb;
+  }
+
+  .listbox [role='option']:hover {
+    background: rgba(240, 240, 240);
+  }
+
+  .listbox [role='option'].focused:hover {
+    background: #fcb2be;
+  }
+</style>
---
+++ src/components/listbox/SelectionListboxToolbar.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  export let moveAllToTheLeftDisabled: boolean;
+  export let moveUpDisabled: boolean;
+  export let moveDownDisabled: boolean;
+  export let moveLeftDisabled: boolean;
+  export let moveRightDisabled: boolean;
+  export let moveAllToTheRightDisabled: boolean;
+
+  const dispatcher = createEventDispatcher<{
+    moveAllToTheLeft: void;
+    moveUp: void;
+    moveDown: void;
+    moveLeft: void;
+    moveRight: void;
+    moveAllToTheRight: void;
+  }>();
+</script>
+
+<div class="selection-listbox-toolbar {$$props.class ?? ''}">
+  <button
+    title="Move Up (Alt + Up arrow key)"
+    type="button"
+    disabled={moveUpDisabled}
+    on:click={() => dispatcher('moveUp')}>👆</button
+  >
+  <button
+    title="Move down (Alt + Down arrow key)"
+    type="button"
+    disabled={moveDownDisabled}
+    on:click={() => dispatcher('moveDown')}>👇</button
+  >
+  <button
+    title="Move left (Space)"
+    type="button"
+    disabled={moveLeftDisabled}
+    on:click={() => dispatcher('moveLeft')}>👈</button
+  >
+  <button
+    title="Move right (Space)"
+    type="button"
+    disabled={moveRightDisabled}
+    on:click={() => dispatcher('moveRight')}>👉</button
+  >
+  <button
+    title="Move all to the left"
+    type="button"
+    disabled={moveAllToTheLeftDisabled}
+    on:click={() => dispatcher('moveAllToTheLeft')}>◀◀</button
+  >
+  <button
+    title="Move all to the right"
+    type="button"
+    disabled={moveAllToTheRightDisabled}
+    on:click={() => dispatcher('moveAllToTheRight')}>▶▶</button
+  >
+</div>
+
+<style>
+  .selection-listbox-toolbar {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .selection-listbox-toolbar button {
+    padding: 0.5rem;
+  }
+</style>
