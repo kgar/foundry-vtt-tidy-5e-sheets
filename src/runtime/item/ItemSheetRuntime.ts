b947471313c0c96c574cd9adca0716fc24d2894e
Kevin Garner <kevinleegarner@gmail.com>
2023-12-08 20:50:51 -0600

    Correlated sheet components and tabs in ItemSheetRuntime.

--- src/runtime/item/ItemSheetRuntime.ts
+++ src/runtime/item/ItemSheetRuntime.ts
@@ -1,6 +1,22 @@
 import type { RegisterItemDetailsSectionOptions } from 'src/api/api.types';
 import type { TabBase } from 'src/api/tab/TabBase';
+import { CONSTANTS } from 'src/constants';
 import { isNil } from 'src/utils/data';
+import type { SvelteComponent } from 'svelte';
+import EquipmentSheet from 'src/sheets/item/EquipmentSheet.svelte';
+import type { Tab } from 'src/types/types';
+import itemSheetTabs from './item-sheet-tabs';
+import BackgroundSheet from 'src/sheets/item/BackgroundSheet.svelte';
+import BackpackSheet from 'src/sheets/item/BackpackSheet.svelte';
+import ClassSheet from 'src/sheets/item/ClassSheet.svelte';
+import ConsumableSheet from 'src/sheets/item/ConsumableSheet.svelte';
+import FeatSheet from 'src/sheets/item/FeatSheet.svelte';
+import LootSheet from 'src/sheets/item/LootSheet.svelte';
+import SpellSheet from 'src/sheets/item/SpellSheet.svelte';
+import SubclassSheet from 'src/sheets/item/SubclassSheet.svelte';
+import ToolSheet from 'src/sheets/item/ToolSheet.svelte';
+import WeaponSheet from 'src/sheets/item/WeaponSheet.svelte';
+import RaceSheet from 'src/sheets/item/RaceSheet.svelte';
 
 export class ItemSheetRuntime {
   private static _itemDetailCustomSections: RegisterItemDetailsSectionOptions[] =
@@ -29,9 +45,103 @@ export class ItemSheetRuntime {
     ItemSheetRuntime._itemTabs.push(tab);
   }
 
-  static getTabs(context: any) {
+  static getCustomItemTabs(context: any) {
     return ItemSheetRuntime._itemTabs.filter(
       (s) => s.enabled === undefined || s.enabled(context)
     );
   }
+
+  static sheets: Record<string, ItemSheetInfo> = {
+    [CONSTANTS.ITEM_TYPE_EQUIPMENT]: {
+      Sheet: EquipmentSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.equipmentDetails,
+        itemSheetTabs.effects,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_BACKGROUND]: {
+      Sheet: BackgroundSheet,
+      defaultTabs: [itemSheetTabs.description, itemSheetTabs.advancement],
+    },
+    [CONSTANTS.ITEM_TYPE_BACKPACK]: {
+      Sheet: BackpackSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.backpackDetails,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_CLASS]: {
+      Sheet: ClassSheet,
+      defaultTabs: [
+        itemSheetTabs.description,
+        itemSheetTabs.classDetails,
+        itemSheetTabs.advancement,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_CONSUMABLE]: {
+      Sheet: ConsumableSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.consumableDetails,
+        itemSheetTabs.effects,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_FEAT]: {
+      Sheet: FeatSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.featDetails,
+        itemSheetTabs.effects,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_LOOT]: {
+      Sheet: LootSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.lootDetails,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_SPELL]: {
+      Sheet: SpellSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.spellDetails,
+        itemSheetTabs.effects,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_SUBCLASS]: {
+      Sheet: SubclassSheet,
+      defaultTabs: [
+        itemSheetTabs.description,
+        itemSheetTabs.subclassDetails,
+        itemSheetTabs.advancement,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_TOOL]: {
+      Sheet: ToolSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.toolDetails,
+        itemSheetTabs.effects,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_WEAPON]: {
+      Sheet: WeaponSheet,
+      defaultTabs: [
+        itemSheetTabs.descriptionWithSidebar,
+        itemSheetTabs.weaponDetails,
+        itemSheetTabs.effects,
+      ],
+    },
+    [CONSTANTS.ITEM_TYPE_RACE]: {
+      Sheet: RaceSheet,
+      defaultTabs: [itemSheetTabs.raceDescription, itemSheetTabs.advancement],
+    },
+  };
 }
+
+type ItemSheetInfo = {
+  Sheet: SvelteComponentConstructor<any, any>;
+  defaultTabs: Tab[];
+};
