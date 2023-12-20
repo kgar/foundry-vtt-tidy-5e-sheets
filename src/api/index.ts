b3ed91f07b1f4f2d10a89a7f4e341f0d0feb98e6
Kevin Lee Garner <kgar@users.noreply.github.com>
2023-12-20 17:11:28 -0600

    Added exhaustion API functions and documented them. (#196)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 export * from './api.types';
 export * from './Tidy5eSheetsApi';
 export * from './item-summary/ItemSummaryApi';
+export * from './exhaustion/ExhaustionApi';
 export * from './tab/CustomTabBase';
 export * from './tab/HtmlTab';
 export * from './tab/HandlebarsTab';
