f9d3cbc7986875845fff02a4cd3dadb65c74db34
Kevin Garner <kevinleegarner@gmail.com>
2024-02-03 00:24:48 -0600

    Added some breathing room.

--- src/api/config/actor-traits/types.ts
+++ src/api/config/actor-traits/types.ts
@@ -5,14 +5,19 @@
 export interface CustomActorTrait {
   /** The primary title text to display for this trait. */
   title: string;
+
   /** Empty traits can be collapsed. Set to `true` to never hide this trait. Default: `false`. */
   alwaysShow?: boolean;
+
   /** Handler for when the configuration command is executed. This could be when a user clicks an edit/configuration button on the sheet. */
   openConfiguration?: (params: CustomTraitOpenConfigurationParams) => void;
+
   /** The tooltip to show when hovering over the trait configuration button. */
   openConfigurationTooltip?: string;
+
   /** An optional callback to determine if this trait should show for the target sheet. When this callback is excluded, it is considered enabled by default. When enabled returns `false`, the trait does not render to the sheet. */
   enabled?: (params: CustomTraitEnabledParams) => boolean;
+
   /** An optional class for an `<i>` element to render an icon. Currently cupports FontAwesome and RPG Awesome. */
   iconClass?: string;
 }
@@ -24,10 +29,13 @@ export interface CustomActorTrait {
 export interface CustomTraitOpenConfigurationParams {
   /** The target sheet for the trait. */
   app: any;
+
   /** The sheet's HTML element. */
   element: HTMLElement;
+
   /** The actor sheet context data that is typically provided on render. */
   data: any;
+
   /** Any inciting event by the user. Usually, this will be a click or pointer event. */
   event: Event;
 }
