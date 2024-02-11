4641bbdf354e8845df86b5507326b548248b94f1
Kevin Garner <kevinleegarner@gmail.com>
2024-02-10 18:03:15 -0600

    Prototyped the raw spell attack feature.

--- src/components/spellbook/SpellbookFooter.svelte
+++ src/components/spellbook/SpellbookFooter.svelte
@@ -22,6 +22,63 @@
     abbr: a[0],
     ...a[1],
   }));
+
+  function fred(ev: MouseEvent, attackType: 'rsak' | 'msak' = 'rsak') {
+    // TODO: Localize
+    let title = `${$context.actor.name} - Spell Attack Roll`;
+
+    const rollData: Record<string, string> = {};
+    const parts: string[] = [];
+
+    // Ability score modifier
+    const spellcastingAbility = $context.actor.system.attributes.spellcasting;
+    const spellcastingMod =
+      $context.actor.system.abilities[spellcastingAbility]?.mod;
+    if (spellcastingAbility !== 'none' && spellcastingMod) {
+      parts.push('@mod');
+      rollData.mod = spellcastingMod;
+    }
+
+    // Add proficiency bonus.
+    parts.push('@prof');
+    rollData.prof = $context.actor.system.attributes.prof;
+
+    // Actor-level global bonus to attack rolls
+    const actorBonusAttack =
+      $context.actor.system.bonuses?.[attackType]?.attack;
+    if (actorBonusAttack) {
+      parts.push(actorBonusAttack);
+    }
+
+    const rollConfig = foundry.utils.mergeObject(
+      {
+        actor: $context.actor,
+        data: rollData,
+        critical: $context.actor.flags['dnd5e']?.spellCriticalThreshold,
+        title,
+        flavor: title,
+        elvenAccuracy: $context.actor.flags['dnd5e']?.elvenAccuracy ?? false,
+        halflingLucky: $context.actor.flags['dnd5e']?.halflingLucky ?? false,
+        dialogOptions: {
+          width: 400,
+          top: ev ? ev.clientY - 80 : null,
+          left: ev ? ev.clientX + 40 : null,
+        },
+        messageData: {
+          'flags.dnd5e.roll': {
+            type: 'attack',
+            // itemId: this.id,
+            // itemUuid: this.uuid,
+          },
+          speaker: ChatMessage.getSpeaker({ actor: $context.actor }),
+        },
+        event: ev,
+      },
+      {},
+    );
+    rollConfig.parts = parts;
+    dnd5e.dice.d20Roll(rollConfig);
+  }
 </script>
 
 <TabFooter cssClass="{cssClass} spellbook-footer" mode="horizontal">
@@ -42,11 +99,14 @@
         <span>{FoundryAdapter.localize('TIDY5E.AttackMod')}:</span>
 
         {#if $context.spellCalculations.rangedMod !== $context.spellCalculations.meleeMod}
-          <div
+          <button
+            type="button"
+            on:click={(ev) => fred(ev, 'rsak')}
+            tabindex={$settingStore.useAccessibleKeyboardSupport ? 0 : -1}
             data-tooltip="{FoundryAdapter.localize(
               'TIDY5E.RangedSpellAttackMod',
             )}: {$context.spellCalculations.rangedTooltip}"
-            class="spell-attack-mod-container"
+            class="inline-transparent-button spell-attack-mod-button"
           >
             <i class="fa-solid fa-wand-magic-sparkles"></i>
             <span
@@ -58,12 +118,15 @@
             >
               {$context.spellCalculations.rangedMod}
             </span>
-          </div>
-          <div
+          </button>
+          <button
+            type="button"
+            on:click={(ev) => fred(ev, 'msak')}
+            tabindex={$settingStore.useAccessibleKeyboardSupport ? 0 : -1}
             data-tooltip="{FoundryAdapter.localize(
               'TIDY5E.MeleeSpellAttackMod',
             )}: {$context.spellCalculations.meleeTooltip}"
-            class="spell-attack-mod-container"
+            class="inline-transparent-button spell-attack-mod-button"
           >
             <i class="fa-solid fa-hand-sparkles"></i>
             <span
@@ -74,13 +137,16 @@
             >
               {$context.spellCalculations.meleeMod}
             </span>
-          </div>
+          </button>
         {:else}
-          <div
+          <button
+            type="button"
+            on:click={(ev) => fred(ev)}
+            tabindex={$settingStore.useAccessibleKeyboardSupport ? 0 : -1}
             data-tooltip="{FoundryAdapter.localize(
               'TIDY5E.SpellAttackMod',
             )}: {$context.spellCalculations.rangedTooltip}"
-            class="spell-attack-mod-container"
+            class="inline-transparent-button spell-attack-mod-button"
           >
             <span
               class="spell-attack-mod"
@@ -90,7 +156,7 @@
             >
               {$context.spellCalculations.rangedMod}
             </span>
-          </div>
+          </button>
         {/if}
       {/if}
     </div>
@@ -160,14 +226,24 @@
     font-size: 0.75rem;
   }
 
-  .spell-attack-mod-container,
+  .spell-attack-mod-button,
   .dc-container {
     display: flex;
     align-items: center;
     gap: 0.125rem;
   }
 
-  .spell-attack-mod-container i {
+  .spell-attack-mod-button {
+    font-family: var(--t5e-title-font-family);
+    font-weight: 700;
+
+    // TODO: Use rollable?
+    &:hover {
+      color: var(--t5e-primary-accent-color);
+    }
+  }
+
+  .spell-attack-mod-button i {
     font-size: 1rem;
   }
 
