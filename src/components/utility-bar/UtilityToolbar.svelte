c976828559bd7462b0df9229d13d54e9ffa74702
Kevin Garner <kevinleegarner@gmail.com>
2024-02-24 16:09:11 -0600

    Removed prototype utility toolbar component logic.

--- src/components/utility-bar/UtilityToolbar.svelte
+++ src/components/utility-bar/UtilityToolbar.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
   import { CONSTANTS } from 'src/constants';
-
-  export let fred = true;
 </script>
 
 <section
   class="utility-toolbar {$$props.class ?? ''}"
-  class:fred
   data-tidy-sheet-part={CONSTANTS.SHEET_PARTS.UTILITY_TOOLBAR}
 >
   <slot />
@@ -19,15 +16,12 @@
     gap: 0.5rem;
     align-items: center;
     border-bottom: 0.0625rem solid var(--t5e-separator-color);
-
-    &.fred {
-      margin-bottom: 0.25rem;
-      margin-left: -1rem;
-      margin-right: -0.25rem;
-      margin-top: calc(var(--vertical-padding) - var(--t5e-tab-padding-top));
-      padding-bottom: var(--vertical-padding);
-      padding-left: 1rem;
-      padding-right: 1.75rem;
-    }
+    margin-bottom: 0.25rem;
+    margin-left: -1rem;
+    margin-right: -0.25rem;
+    margin-top: calc(var(--vertical-padding) - var(--t5e-tab-padding-top));
+    padding-bottom: var(--vertical-padding);
+    padding-left: 1rem;
+    padding-right: 1.75rem;
   }
 </style>
