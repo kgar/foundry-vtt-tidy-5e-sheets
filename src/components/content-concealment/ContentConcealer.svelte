8102e1864246dc06727030d3ee3c5d95361c8e6c
Kevin Garner <kevin.garner@ssigroup.com>
2024-02-28 17:16:58 -0600

    Added a content concealer.
    Added unidentified notice.
    
    Added general item styles for concealing information.
    
    Prototyped content concealment with the item loot details tab.

---
+++ src/components/content-concealment/ContentConcealer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import UnidentifiedNotice from './UnidentifiedNotice.svelte';
+
+  export let conceal: boolean;
+</script>
+
+<div role="presentation" class="obscurable-parent inert-animation-container">
+  <div role="presentation" inert={conceal}>
+    <slot />
+  </div>
+  {#if conceal}
+    <UnidentifiedNotice />
+  {/if}
+</div>
+
+<style lang="scss">
+  .obscurable-parent {
+    flex: 1;
+    position: relative;
+  }
+</style>
--- src/sheets/item/tabs/ItemLootDetailsTab.svelte
+++ src/sheets/item/tabs/ItemLootDetailsTab.svelte
@@ -7,59 +7,62 @@
   import Select from 'src/components/inputs/Select.svelte';
   import SelectOptions from 'src/components/inputs/SelectOptions.svelte';
   import ItemProperties from '../parts/ItemProperties.svelte';
+  import ContentConcealer from 'src/components/content-concealment/ContentConcealer.svelte';
 
   let context = getContext<Readable<ItemSheetContext>>('context');
 
   const localize = FoundryAdapter.localize;
 </script>
 
-<h3 class="form-header">{localize('DND5E.ItemLootDetails')}</h3>
+<ContentConcealer conceal={$context.concealDetails}>
+  <h3 class="form-header">{localize('DND5E.ItemLootDetails')}</h3>
 
-<ItemFormGroup
-  labelText={localize('DND5E.ItemLootType')}
-  field="system.type.value"
-  let:inputId
->
-  <Select
-    id={inputId}
-    document={$context.item}
-    field="system.type.value"
-    value={$context.system.type.value}
-    disabled={!$context.editable}
-  >
-    <SelectOptions
-      data={$context.config.lootTypes}
-      labelProp="label"
-      blank=""
-    />
-  </Select>
-</ItemFormGroup>
-
-{#if $context.lootSubtypes}
-  {@const subTypeLabel = localize('DND5E.ItemLootSubtype', {
-    category:
-      $context.config.lootTypes[$context.system.type.value]?.label ?? '',
-  })}
   <ItemFormGroup
-    labelText={subTypeLabel}
-    field="system.type.subtype"
+    labelText={localize('DND5E.ItemLootType')}
+    field="system.type.value"
     let:inputId
   >
     <Select
       id={inputId}
       document={$context.item}
-      field="system.type.subtype"
-      value={$context.system.type.subtype}
+      field="system.type.value"
+      value={$context.system.type.value}
       disabled={!$context.editable}
     >
-      <SelectOptions data={$context.lootSubtypes} blank="" />
+      <SelectOptions
+        data={$context.config.lootTypes}
+        labelProp="label"
+        blank=""
+      />
     </Select>
   </ItemFormGroup>
-{/if}
 
-<ItemFormGroup
-  cssClass="stacked loot-properties"
-  labelText={localize('DND5E.ItemLootProperties')}
->
-  <ItemProperties />
-</ItemFormGroup>
+  {#if $context.lootSubtypes}
+    {@const subTypeLabel = localize('DND5E.ItemLootSubtype', {
+      category:
+        $context.config.lootTypes[$context.system.type.value]?.label ?? '',
+    })}
+    <ItemFormGroup
+      labelText={subTypeLabel}
+      field="system.type.subtype"
+      let:inputId
+    >
+      <Select
+        id={inputId}
+        document={$context.item}
+        field="system.type.subtype"
+        value={$context.system.type.subtype}
+        disabled={!$context.editable}
+      >
+        <SelectOptions data={$context.lootSubtypes} blank="" />
+      </Select>
+    </ItemFormGroup>
+  {/if}
+
+  <ItemFormGroup
+    cssClass="stacked loot-properties"
+    labelText={localize('DND5E.ItemLootProperties')}
+  >
+    <ItemProperties />
+  </ItemFormGroup>
+</ContentConcealer>
