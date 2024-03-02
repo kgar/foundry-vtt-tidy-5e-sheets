f7c7957cf4d823ec5bcd06602116c314bc5ad1e4
Kevin Garner <kevinleegarner@gmail.com>
2024-03-01 21:45:39 -0600

    Added sheet part and metadata to condition toggles.

--- src/components/toggle/ConditionToggle.svelte
+++ src/components/toggle/ConditionToggle.svelte
@@ -7,6 +7,7 @@
   import Dnd5eIcon from 'src/components/icon/Dnd5eIcon.svelte';
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
   import { debug, error } from 'src/utils/logging';
+    import { CONSTANTS } from 'src/constants';
 
   const context = getContext<Readable<ActorSheetContext>>('context');
 
@@ -40,6 +41,9 @@
   on:change={(ev) => handleChange(ev.detail.originalValue)}
   title={condition.name}
   disabled={!$context.editable}
+  data-uuid={condition.reference}
+  data-condition-id={condition.id}
+  data-tidy-sheet-part={CONSTANTS.SHEET_PARTS.CONDITION_TOGGLE}
 >
   <Dnd5eIcon src={condition.icon} />
   <span class="flex-1 truncate">{condition.name}</span>
