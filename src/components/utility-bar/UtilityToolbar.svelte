5ff8e1348151bd7609e39454bf64d7fde24a809d
Kevin Garner <kevinleegarner@gmail.com>
2024-03-09 00:17:57 -0600

    Made UtilityToolbar margin/padding overrideable via CSS variables.

--- src/components/utility-bar/UtilityToolbar.svelte
+++ src/components/utility-bar/UtilityToolbar.svelte
@@ -11,17 +11,22 @@
 
 <style lang="scss">
   .utility-toolbar {
-    --vertical-padding: 0.375rem;
+    --vertical-padding-internal: 0.375rem;
     display: flex;
     gap: 0.5rem;
     align-items: center;
     border-bottom: 0.0625rem solid var(--t5e-separator-color);
-    margin-bottom: 0.25rem;
-    margin-left: -1rem;
-    margin-right: -0.25rem;
-    margin-top: calc(var(--vertical-padding) - var(--t5e-tab-contents-padding-top));
-    padding-bottom: var(--vertical-padding);
-    padding-left: 1rem;
-    padding-right: 1.75rem;
+    margin-bottom: var(--utility-toolbar-margin-bottom, 0.25rem);
+    margin-left: var(--utility-toolbar-margin-left, -1rem);
+    margin-right: var(--utility-toolbar-margin-right, -0.25rem);
+    margin-top: var(
+      --utility-toolbar-margin-top,
+      calc(
+        var(--vertical-padding-internal) - var(--t5e-tab-contents-padding-top)
+      )
+    );
+    padding-bottom: var(--vertical-padding-internal);
+    padding-left: var(--utility-toolbar-padding-left, 1rem);
+    padding-right: var(--utility-toolbar-padding-right, 1.75rem);
   }
 </style>
