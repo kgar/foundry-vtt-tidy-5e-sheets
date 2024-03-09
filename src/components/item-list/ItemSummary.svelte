8933e5712fb55ab9fbd79635cfe2d22b2ed8ae9c
Kevin Garner <kevinleegarner@gmail.com>
2024-03-09 15:58:36 -0600

    Tightened up the bottom border for item summary.

--- src/components/item-list/ItemSummary.svelte
+++ src/components/item-list/ItemSummary.svelte
@@ -54,7 +54,7 @@
     flex: 0 0 100%;
     padding-top: var(--item-summary-padding-top, 0);
     padding-right: var(--item-summary-padding-right, 0.5rem);
-    padding-bottom: var(--item-summary-padding-bottom, 0.5rem);
+    padding-bottom: var(--item-summary-padding-bottom, 0.25rem);
     padding-left: var(--item-summary-padding-left, 0.5rem);
     border-top: 0.0625rem solid var(--t5e-faint-color);
     font-size: 0.75rem;
