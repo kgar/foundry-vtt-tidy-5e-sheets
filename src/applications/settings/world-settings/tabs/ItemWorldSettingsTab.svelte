58d6e111815183d94d23c653cbcd3429034dad6e
Kevin Garner <kevin.garner@ssigroup.com>
2024-03-12 16:21:00 -0500

    Trimmed up new item tab component.

--- src/applications/settings/world-settings/tabs/ItemWorldSettingsTab.svelte
+++ src/applications/settings/world-settings/tabs/ItemWorldSettingsTab.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-    import { FoundryAdapter } from 'src/foundry/foundry-adapter';
-    import { getContext } from 'svelte';
-    import { SettingsProvider } from 'src/settings/settings';
-    import SelectSetting from 'src/applications/settings/parts/SelectSetting.svelte';
-    import type {
-      WorldSettingsContextStore,
-      WorldSettingsFunctions,
-    } from '../WorldSettings.types';
-  
-    const context = getContext<WorldSettingsContextStore>('context');
-  
-    const localize = FoundryAdapter.localize;
-  </script>
+  import { getContext } from 'svelte';
+  import { SettingsProvider } from 'src/settings/settings';
+  import SelectSetting from 'src/applications/settings/parts/SelectSetting.svelte';
+  import type { WorldSettingsContextStore } from '../WorldSettings.types';
+
+  const context = getContext<WorldSettingsContextStore>('context');
+</script>
 
 <div class="settings-form">
   <SelectSetting
-    options={SettingsProvider.settings.itemIdentificationPermission.options.choices}
+    options={SettingsProvider.settings.itemIdentificationPermission.options
+      .choices}
     bind:value={$context.settings.itemIdentificationPermission}
     name={SettingsProvider.settings.itemIdentificationPermission.options.name}
     hint={SettingsProvider.settings.itemIdentificationPermission.options.hint}
