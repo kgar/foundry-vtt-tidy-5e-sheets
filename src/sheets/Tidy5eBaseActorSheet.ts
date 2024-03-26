bba1ea34ec5cfb03807325f3bacb6ad9dea1925e
Kevin Garner <kevinleegarner@gmail.com>
2024-03-25 19:31:17 -0500

    Hardened DM prep.

--- src/sheets/Tidy5eBaseActorSheet.ts
+++ src/sheets/Tidy5eBaseActorSheet.ts
@@ -3,6 +3,7 @@ import type {
   DamageModificationContextEntry,
   DamageModificationData,
 } from 'src/types/types';
+import { debug, error } from 'src/utils/logging';
 
 /**
  * Temporarily a utility class for common actor sheet functions.
@@ -15,29 +16,38 @@ export class Tidy5eBaseActorSheet {
   }
 
   static applyDamageModifications(context: ActorSheetContext) {
-    // Damage modifications
-    const dm: DamageModificationData | undefined =
-      context.actor.system.traits?.dm;
-    if (dm) {
-      const rollData = context.actor.getRollData({ deterministic: true });
-      const mods = Object.entries(dm.amount)
-        .map(([key, value]) => {
-          const total = dnd5e.utils.simplifyBonus(value, rollData);
-          if (!total) return null;
-          const mod: DamageModificationContextEntry = {
-            label: `${
-              CONFIG.DND5E.damageTypes[key]?.label ?? key
-            } ${dnd5e.utils.formatNumber(total, { signDisplay: 'always' })}`,
-            consequence: total > 0 ? 'detriment' : 'benefit',
-          };
-          const icons: string[] = (mod.icons = []);
-          if (dm.bypasses.size && CONFIG.DND5E.damageTypes[key]?.isPhysical)
-            icons.push(...dm.bypasses);
-          return mod;
-        })
-        .filter((f) => f);
+    try {
+      // Damage modifications
+      const dm: DamageModificationData | undefined =
+        context.actor.system.traits?.dm;
+      if (dm) {
+        const rollData = context.actor.getRollData({ deterministic: true });
+        const mods = Object.entries(dm.amount)
+          .map(([key, value]) => {
+            const total = dnd5e.utils.simplifyBonus(value, rollData);
+            if (!total) return null;
+            const mod: DamageModificationContextEntry = {
+              label: `${
+                CONFIG.DND5E.damageTypes[key]?.label ?? key
+              } ${dnd5e.utils.formatNumber(total, { signDisplay: 'always' })}`,
+              consequence: total > 0 ? 'detriment' : 'benefit',
+            };
+            const icons: string[] = (mod.icons = []);
+            if (dm.bypasses.size && CONFIG.DND5E.damageTypes[key]?.isPhysical)
+              icons.push(...dm.bypasses);
+            return mod;
+          })
+          .filter((f) => f);
 
-      context.traits.traits.dm = mods;
+        context.traits.traits.dm = mods;
+      }
+    } catch (e) {
+      error(
+        'An error occurred while preparing Damage Modification data',
+        false,
+        e
+      );
+      debug('Damage Modification error troubleshooting info', { context });
     }
   }
 }
