65c984db727a31bdb2178c1ee59454a781747ee5
Kevin Garner <kevin.garner@ssigroup.com>
2024-03-28 12:51:27 -0500

    Began to flesh out ApplyTidySheetPreferencesApplication and created corresponding component.

---
+++ src/applications/sheet-preferences/ApplyTidySheetPreferences.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import {
+    ApplyTidySheetPreferencesApplication,
+    type SheetPreferenceOption,
+  } from './ApplyTidySheetPreferencesApplication';
+  import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+
+  export let options: SheetPreferenceOption[];
+
+  const localize = FoundryAdapter.localize;
+</script>
+
+<ul>
+  {#each options as option (option.identifier)}
+    <li>
+      {option.identifier} - {option.label} - {option.selected}
+    </li>
+  {/each}
+</ul>
+<button
+  on:click={() =>
+    ApplyTidySheetPreferencesApplication.applyPreferences(options)}
+  >{localize('TIDY5E.ButtonConfirm.Text')}</button
+>
--- src/applications/sheet-preferences/ApplyTidySheetPreferencesApplication.ts
+++ src/applications/sheet-preferences/ApplyTidySheetPreferencesApplication.ts
@@ -1,8 +1,46 @@
 import type { SvelteComponent } from 'svelte';
 import SvelteFormApplicationBase from '../SvelteFormApplicationBase';
+import ApplyTidySheetPreferences from './ApplyTidySheetPreferences.svelte';
+import { writable } from 'svelte/store';
+
+export type SheetPreferenceOption = {
+  label: string;
+  identifier: string;
+  settingKey: string;
+  selected: boolean;
+};
 
 export class ApplyTidySheetPreferencesApplication extends SvelteFormApplicationBase {
-    createComponent(node: HTMLElement): SvelteComponent<any, any, any> {
-        throw new Error('Method not implemented.');
+  createComponent(node: HTMLElement): SvelteComponent<any, any, any> {
+    return new ApplyTidySheetPreferences({
+      target: node,
+      props: {
+        options:
+          ApplyTidySheetPreferencesApplication.getTidySheetPreferenceOptions(),
+      },
+    });
+  }
+
+  static getTidySheetPreferenceOptions() {
+    const types: SheetPreferenceOption[] = [];
+    for (const cls of Object.values<any>(foundry.documents)) {
+      const documentName = cls.documentName;
+      if (!cls.hasTypeData) continue;
+      const subTypes = game.documentTypes[documentName].filter(
+        (t: any) => t !== CONST.BASE_DOCUMENT_TYPE
+      );
+      if (!subTypes.length) continue;
+      const title = game.i18n.localize(cls.metadata.labelPlural);
+      subTypes.forEach((t: any) => {
+        const typeLabel = CONFIG[documentName].typeLabels?.[t];
+        const name = typeLabel ? game.i18n.localize(typeLabel) : t;
+        const { defaultClasses, defaultClass } =
+          DocumentSheetConfig.getSheetClassesForSubType(documentName, t);
+        console.log({ type: t, name, defaultClasses, defaultClass });
+      });
     }
+    return [];
+  }
+
+  static applyPreferences(options: SheetPreferenceOption[]) {}
 }
