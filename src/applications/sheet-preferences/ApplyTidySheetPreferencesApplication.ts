a47e538ff6ee5633d3f87653b4712090bf9c3f98
Kevin Garner <kevinleegarner@gmail.com>
2024-03-28 10:12:15 -0500

    Added localization keys and new menu setting for applying Tidy default sheet preferences.
    Added stub application in preparation to implement.

---
+++ src/applications/sheet-preferences/ApplyTidySheetPreferencesApplication.ts
@@ -0,0 +1,8 @@
+import type { SvelteComponent } from 'svelte';
+import SvelteFormApplicationBase from '../SvelteFormApplicationBase';
+
+export class ApplyTidySheetPreferencesApplication extends SvelteFormApplicationBase {
+    createComponent(node: HTMLElement): SvelteComponent<any, any, any> {
+        throw new Error('Method not implemented.');
+    }
+}
