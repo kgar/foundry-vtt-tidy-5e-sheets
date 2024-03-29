b6f6572096e2f65f1c0f35c5079b747eb20e3cac
Kevin Garner <kevinleegarner@gmail.com>
2024-03-28 23:25:26 -0500

    Introduced key prop for each action filter override.
    Added flags for actor section order.

---
+++ src/features/sections/sections.types.ts
@@ -0,0 +1,5 @@
+type SectionKey = string;
+
+export type ActorSectionOrder = {
+    [tabId: string]: SectionKey[]
+}
--- src/foundry/TidyFlags.ts
+++ src/foundry/TidyFlags.ts
@@ -1,5 +1,7 @@
 import type { Item5e } from 'src/types/item.types';
 import { FoundryAdapter } from './foundry-adapter';
+import type { Actor5e } from 'src/types/types';
+import type { ActorSectionOrder } from 'src/features/sections/sections.types';
 
 /** Manages Tidy flags. */
 export class TidyFlags {
@@ -9,23 +11,59 @@ export class TidyFlags {
    * `undefined` means there is no override and standard Action List logic should be used.
    * */
   static actionFilterOverride = {
-    /** Get the item's Action Filter Override setting. */
+    key: 'action-filter-override',
+    /** Gets the item's Action Filter Override setting. */
     get(item: Item5e): boolean | undefined {
       return (
-        FoundryAdapter.tryGetFlag<boolean>(item, 'action-filter-override') ??
-        undefined
+        FoundryAdapter.tryGetFlag<boolean>(
+          item,
+          TidyFlags.actionFilterOverride.key
+        ) ?? undefined
       );
     },
-    /** Set the item's Action Filter Override setting. */
+    /** Sets the item's Action Filter Override setting. */
     set(item: Item5e, value: boolean): Promise<void> {
-      return FoundryAdapter.setFlag(item, 'action-filter-override', value);
+      return FoundryAdapter.setFlag(
+        item,
+        TidyFlags.actionFilterOverride.key,
+        value
+      );
     },
     /**
-     * Clear the item's Action Filter Override setting,
+     * Clears the item's Action Filter Override setting,
      * meaning the item should use the standard Action List logic
      * for inclusion or exclusion. */
     unset(item: Item5e) {
-      return FoundryAdapter.unsetFlag(item, 'action-filter-override');
+      return FoundryAdapter.unsetFlag(item, TidyFlags.actionFilterOverride.key);
+    },
+  };
+
+  /** Determines the order of item table sections for a given actor. */
+  static actorSectionOrder = {
+    key: 'actor-section-order',
+    /** Gets the actor's section ordering. `undefined` means to use the default order.  */
+    get(actor: Actor5e): ActorSectionOrder | undefined {
+      return (
+        FoundryAdapter.tryGetFlag<ActorSectionOrder>(
+          actor,
+          TidyFlags.actorSectionOrder.key
+        ) ?? undefined
+      );
+    },
+    /** Sets the actor's section ordering. */
+    set(actor: Actor5e, value: ActorSectionOrder) {
+      return FoundryAdapter.setFlag(
+        actor,
+        TidyFlags.actorSectionOrder.key,
+        value
+      );
+    },
+    /**
+     * Clears the actor's section ordering,
+     * meaning the target actor should use default ordering.
+     * */
+    unset(actor: Actor5e) {
+      return FoundryAdapter.unsetFlag(actor, TidyFlags.actorSectionOrder.key);
     },
   };
 }
