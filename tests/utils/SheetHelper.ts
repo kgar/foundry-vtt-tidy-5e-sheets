12afebc3c51155a5e7cf3026938dc720fd1b2b3a
Kevin Garner <kevin.garner@ssigroup.com>
2024-04-29 16:56:39 -0500

    Added test types and sheet helpers for handling some common sheet tasks while testing.

---
+++ tests/utils/SheetHelper.ts
@@ -0,0 +1,33 @@
+import type { Locator, Page } from '@playwright/test';
+import { CONSTANTS } from 'src/constants';
+import type { DocumentRef } from 'tests/tests.types';
+
+export class SheetHelper {
+  page: Page;
+  ref: DocumentRef;
+  $sheet: Locator;
+
+  constructor(page: Page, ref: DocumentRef) {
+    this.page = page;
+    this.ref = ref;
+    this.$sheet = page.locator(
+      `[data-sheet-module="${CONSTANTS.MODULE_ID}"][id*=${ref.id}]`
+    );
+  }
+
+  async showSheet() {
+    await this.page.evaluate(
+      async ({ ref }) => {
+        const actor = await fromUuid(ref.uuid);
+        actor.sheet.render();
+        actor.sheet.bringToFront();
+      },
+      { ref: this.ref }
+    );
+  }
+
+  async tab(tabId: string) {
+    const $tab = this.$sheet.locator(`nav [data-tab-id=${tabId}]`);
+    await $tab.click();
+  }
+}
