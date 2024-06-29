26c008846ed490b7db2e13ad5bfed792fa6955df
Kevin Garner <kevinleegarner@gmail.com>
2024-06-29 16:23:21 -0500

    Added missing prop for recursive containers.

--- src/sheets/item/parts/ContainerContentsList.svelte
+++ src/sheets/item/parts/ContainerContentsList.svelte
@@ -200,6 +200,7 @@
                     {editable}
                     {itemContext}
                     {lockItemQuantity}
+                    {inlineContainerService}
                   />
                 </div>
               </ExpandableContainer>
