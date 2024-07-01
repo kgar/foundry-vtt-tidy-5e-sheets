a64857c80c0c0cf72020ee184744538f9386d26b
Kevin Garner <kevinleegarner@gmail.com>
2024-07-01 13:26:46 -0500

    Added rough draft of Container.applySectionConfigsRecursively() for applying section configs of the given tab all the way down.
    Refactored/cleaned up.

--- src/features/inline-container/Container.ts
+++ src/features/inline-container/Container.ts
@@ -4,32 +4,28 @@ import type {
   Item5e,
 } from 'src/types/item.types';
 import { Inventory } from '../sections/Inventory';
-import type { SheetTabSectionConfigs } from '../sections/sections.types';
 import { FoundryAdapter } from 'src/foundry/foundry-adapter';
 import { CONSTANTS } from 'src/constants';
 import type { CharacterFavorite } from 'src/foundry/dnd5e.types';
+import type { SectionConfig } from '../sections/sections.types';
+import type { SortMode } from 'src/types/types';
+import type { ItemFilterService } from '../filtering/ItemFilterService';
+import { TidyFlags } from 'src/api';
 
 export class Container {
-  static async getContainerContents(
-    item: Item5e,
-    sectionConfigs: SheetTabSectionConfigs
-  ): Promise<ContainerContents> {
+  static async getContainerContents(item: Item5e): Promise<ContainerContents> {
     const containerItems = item.system.contents.values();
 
     return {
       capacity: await item.system.computeCapacity(),
       currency: item.system.currency,
       contents: Inventory.getInventory(containerItems),
-      itemContext: await Container.getContainerItemContext(
-        item,
-        sectionConfigs
-      ),
+      itemContext: await Container.getContainerItemContext(item),
     };
   }
 
   static async getContainerItemContext(
-    container: Item5e,
-    sectionConfigs: SheetTabSectionConfigs
+    container: Item5e
   ): Promise<Record<string, ContainerItemContext>> {
     const itemContext: Record<string, ContainerItemContext> = {};
 
@@ -51,13 +47,43 @@ export class Container {
       }
 
       if (item.type === CONSTANTS.ITEM_TYPE_CONTAINER) {
-        ctx.containerContents = await Container.getContainerContents(
-          item,
-          sectionConfigs
-        );
+        ctx.containerContents = await Container.getContainerContents(item);
       }
     }
 
     return itemContext;
+  }
+
+  static async applySectionConfigsRecursively(
+    container: Item5e,
+    containerContents: ContainerContents,
+    sortMode: SortMode,
+    itemFilterService: ItemFilterService,
+    tabId: string
+  ) {
+    // TODO: Also solve: sort and filter pertain to the tab in which this content is being displayed.
+    const sectionConfigs = TidyFlags.sectionConfig.get(container)?.[tabId];
+    containerContents.contents = Inventory.applyInventorySectionConfigs(
+      containerContents.contents,
+      sectionConfigs,
+      sortMode,
+      itemFilterService,
+      tabId
+    );
+
+    for (const section of containerContents.contents) {
+      for (const item of section.items) {
+        const ctx = containerContents.itemContext[item.id];
+        if ('containerContents' in ctx && !!ctx.containerContents?.contents) {
+          Container.applySectionConfigsRecursively(
+            item,
+            ctx.containerContents,
+            sortMode,
+            itemFilterService,
+            tabId
+          );
+        }
+      }
+    }
   }
 }
