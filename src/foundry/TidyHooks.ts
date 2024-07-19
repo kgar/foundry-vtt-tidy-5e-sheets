bd234761230a6d3e9e2c2f7847f8cd16583eeb2b
Kevin Garner <kevinleegarner@gmail.com>
2024-07-18 23:00:14 -0500

    Standardized TidyHooks docs.
    Applied minor corrections to Tidy5eSheetsApi docs.
    
    Added some quick links and resources to the Tidy 5e Sheets API front page.

--- src/foundry/TidyHooks.ts
+++ src/foundry/TidyHooks.ts
@@ -1,5 +1,4 @@
 import type { Tidy5eSheetsApi } from 'src/api';
-import { CONSTANTS } from 'src/constants';
 import type { ContextMenuPositionInfo } from 'src/context-menu/context-menu.types';
 import type {
   ActiveEffect5e,
@@ -22,10 +21,9 @@ import type { RollConfig, Roll, RestConfiguration } from './dnd5e.types';
 export class TidyHooks {
   /**
    * The active effect context menu has established its options and is about to show.
-   * @param effect the active effect document instance
-   * @param menuItems the menu items for this active effect
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the menu to show, `false` to prevent the default menu from showing
+   * @param {ActiveEffect5e} effect The active effect document instance.
+   * @param {ContextMenuEntry[]} menuItems The menu items for this active effect.
+   * @returns {boolean} `true` to allow the menu to show, `false` to prevent the default menu from showing.
    *
    * @example
    * ```js
@@ -43,11 +41,9 @@ export class TidyHooks {
 
   /**
    * The context menu for the advancements list has been constructed.
-   * @memberof hookEvents
-   * @param {jQuery} html                      The HTML element to which the context options are attached.
-   * @param {ContextMenuEntry[]} entryOptions  The context menu entries.
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the menu to show, `false` to prevent the default menu from showing
+   * @param {jQuery} html The HTML element to which the context options are attached.
+   * @param {ContextMenuEntry[]} contextOptions The context menu entries.
+   * @returns {boolean} `true` to allow the menu to show, `false` to prevent the default menu from showing.
    *
    * @example
    * ```js
@@ -59,16 +55,15 @@ export class TidyHooks {
   static dnd5eGetItemAdvancementContext(
     html: any,
     contextOptions: ContextMenuEntry[]
-  ) {
+  ): boolean {
     return Hooks.call('dnd5e.getItemAdvancementContext', html, contextOptions);
   }
 
   /**
    * The item context menu has established its options and is about to show.
-   * @param item
-   * @param menuItems
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the menu to show, `false` to prevent the default menu from showing
+   * @param {Item5e} item The item document instance.
+   * @param {ContextMenuEntry[]} menuItems The menu items for this item.
+   * @returns {boolean} `true` to allow the menu to show, `false` to prevent the default menu from showing.
    *
    * @example
    * ```js
@@ -86,11 +81,10 @@ export class TidyHooks {
 
   /**
    * A hit die roll is about to occur.
-   * @param actor the affected actor
-   * @param rollConfig the configuration for the roll
-   * @param denomination the roll denomination
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the roll, `false` to prevent it
+   * @param {Actor5e} actor The affected actor.
+   * @param {RollConfig} rollConfig The configuration for the roll.
+   * @param {string} denomination The roll denomination.
+   * @returns {boolean} `true` to allow the roll, `false` to prevent it.
    *
    * @example
    * ```js
@@ -109,12 +103,9 @@ export class TidyHooks {
 
   /**
    * A short rest is about to start.
-   * @function dnd5e.preShortRest
-   * @memberof hookEvents
-   * @param {Actor5e} actor             The actor that is being rested.
-   * @param {RestConfiguration} config  Configuration options for the rest.
-   * @param hookName this hook's registered name
-   * @returns {boolean}                 Explicitly return `false` to prevent the rest from being started.
+   * @param {Actor5e} actor The actor that is being rested.
+   * @param {RestConfiguration} config Configuration options for the rest.
+   * @returns {boolean} Explicitly return `false` to prevent the rest from being started.
    *
    * @example
    * ```js
@@ -123,21 +114,18 @@ export class TidyHooks {
    * });
    * ```
    */
-  static dnd5ePreShortRest(actor: any, config: RestConfiguration): boolean {
+  static dnd5ePreShortRest(actor: Actor5e, config: RestConfiguration): boolean {
     return Hooks.call('dnd5e.preShortRest', actor, config);
   }
 
   /**
    * A hook event that fires after a hit die has been rolled for an Actor, but before updates have been performed.
-   * @function dnd5eRollHitDie
-   * @memberof hookEvents
-   * @param {Actor5e} actor         Actor for which the hit die has been rolled.
-   * @param {Roll} roll             The resulting roll.
+   * @param {Actor5e} actor Actor for which the hit die has been rolled.
+   * @param {Roll} roll The resulting roll.
    * @param {object} updates
-   * @param {object} updates.actor  Updates that will be applied to the actor.
-   * @param {object} updates.class  Updates that will be applied to the class.
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the roll, `false` to prevent it
+   * @param {object} updates.actor Updates that will be applied to the actor.
+   * @param {object} updates.class Updates that will be applied to the class.
+   * @returns {boolean} `true` to allow the roll, `false` to prevent it.
    *
    * @example
    * ```js
@@ -152,9 +140,8 @@ export class TidyHooks {
 
   /**
    * The context menu is about to show for a given item on a sheet.
-   * @param item the item document instance
-   * @param options the mouse event which triggered the context menu
-   * @param hookName this hook's registered name
+   * @param {Item5e} item The item document instance.
+   * @param {object} options The mouse event which triggered the context menu.
    *
    * @example
    * ```js
@@ -172,11 +159,10 @@ export class TidyHooks {
 
   /**
    * An item is about to be used by an actor.
-   * @param item the affected item document instance
-   * @param config any configuration for the item use
-   * @param options any options for the item use
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the item use, `false` to prevent it
+   * @param {Item5e} item The affected item document instance.
+   * @param {any} config Any configuration for the item use.
+   * @param {any} options Any options for the item use.
+   * @returns {boolean} `true` to allow the item use, `false` to prevent it.
    *
    * @example
    * ```js
@@ -195,9 +181,8 @@ export class TidyHooks {
 
   /**
    * A user has hovered off an item, typically from an item table row.
-   * @param event The triggering mouse event.
-   * @param item The item which is no longer hovered.
-   * @param hookName this hook's registered name
+   * @param {Event} event The triggering mouse event.
+   * @param {any} item The item which is no longer hovered.
    *
    * @example
    * ```js
@@ -212,9 +197,8 @@ export class TidyHooks {
 
   /**
    * A user has hovered over an item, typically on an item table row.
-   * @param event The triggering mouse event.
-   * @param item The hovered-over item.
-   * @param hookName this hook's registered name
+   * @param {Event} event The triggering mouse event.
+   * @param {any} item The hovered-over item.
    *
    * @example
    * ```js
@@ -229,10 +213,9 @@ export class TidyHooks {
 
   /**
    * Sheet item/effect sections are about to be configured. Use this hook to inject additional items, sections, etc., or to adjust context data.
-   * @param app the sheet application instance
-   * @param element the sheet HTML element
-   * @param data the data context from `getData()`
-   * @param hookName this hook's registered name
+   * @param {any} app The sheet application instance.
+   * @param {HTMLElement} element The sheet HTML element.
+   * @param {CharacterSheetContext | NpcSheetContext | ContainerSheetContext} data The data context from `getData()`.
    *
    * @example
    * ```js
@@ -253,11 +236,10 @@ export class TidyHooks {
    * An active effect is about to be created for an owning document.
    * This was initiated specifically through Tidy 5e Sheets,
    * whether directly by the user, or indirectly through some user interaction.
-   * @param owner the owning document instance which will receive the created effect
-   * @param createData the data used to create it
-   * @param userId the user who initiated the creation
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the effect creation, `false` to prevent it
+   * @param {any} owner The owning document instance which will receive the created effect.
+   * @param {object} createData The data used to create it.
+   * @param {string} userId The user who initiated the creation.
+   * @returns {boolean} `true` to allow the effect creation, `false` to prevent it.
    *
    * @example
    * ```js
@@ -283,11 +265,10 @@ export class TidyHooks {
    * An item is about to be created for an owning document.
    * This was initiated specifically through Tidy 5e Sheets,
    * whether directly by the user, or indirectly through some user interaction.
-   * @param owner the owning document instance which will receive the created item
-   * @param createData the data used to create it
-   * @param userId the user who initiated the creation
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the item creation, `false` to prevent it
+   * @param {any} owner The owning document instance which will receive the created item.
+   * @param {object} createData The data used to create it.
+   * @param {string} userId The user who initiated the creation.
+   * @returns {boolean} `true` to allow the item creation, `false` to prevent it.
    *
    * @example
    * ```js
@@ -306,10 +287,9 @@ export class TidyHooks {
 
   /**
    * The portrait picker is about to open.
-   * @param context the actor sheet data from `getData()`
-   * @param event the triggering event
-   * @param hookName this hook's registered name
-   * @returns
+   * @param {ActorSheetContext} context The actor sheet data from `getData()`.
+   * @param {MouseEvent & { currentTarget: EventTarget & HTMLElement }} event The triggering event.
+   * @returns {boolean} `true` to allow the picker to open, `false` to prevent it.
    *
    * @example
    * ```js
@@ -331,9 +311,8 @@ export class TidyHooks {
 
   /**
    * The floating context menu's positioning is being determined.
-   * @param positionInfo context menu positioning info, used for determining where to put the context menu
-   * @param hookName this hook's registered name
-   * @returns `true` to allow the menu to show, `false` to prevent the default menu from showing
+   * @param {ContextMenuPositionInfo} positionInfo Context menu positioning info, used for determining where to put the context menu.
+   * @returns {boolean} `true` to allow the menu to show, `false` to prevent the default menu from showing.
    *
    * @example
    * ```js
@@ -353,9 +332,8 @@ export class TidyHooks {
 
   /**
    * Resources have been prepared. Use this hook to adjust them and/or add new resources.
-   * @param tidyResources the resources that have been prepared for the sheet
-   * @param actor the affected actor
-   * @param hookName this hook's registered name
+   * @param {TidyResource[]} tidyResources The resources that have been prepared for the sheet.
+   * @param {Actor5e} actor The affected actor.
    *
    * @example
    * ```js
@@ -373,11 +351,10 @@ export class TidyHooks {
 
   /**
    * A tab is about to be selected.
-   * @param app the sheet application instances
-   * @param element the sheet's HTML element
-   * @param data the current tab ID before selecting the new tab, and the new tab ID to be selected
-   * @param hookName this hook's registered name
-   * @returns `false` to cancel tabbing
+   * @param {any} app The sheet application instance.
+   * @param {HTMLElement} element The sheet's HTML element.
+   * @param {object} data The current tab ID before selecting the new tab, and the new tab ID to be selected.
+   * @returns {boolean} `false` to cancel tabbing.
    *
    * @example
    * ```js
@@ -396,8 +373,7 @@ export class TidyHooks {
 
   /**
    * Tidy 5e Sheets is ready to be used and customized.
-   * @param api The Tidy 5e Sheets API, used for customizing and registering content with Tidy 5e Sheets.
-   * @param hookName this hook's registered name
+   * @param {Tidy5eSheetsApi} api The Tidy 5e Sheets API, used for customizing and registering content with Tidy 5e Sheets.
    *
    * @example
    * ```js
@@ -411,12 +387,11 @@ export class TidyHooks {
   }
 
   /**
-   *
-   * @param app the sheet application instance
-   * @param element the sheet HTML element
-   * @param data the data context from `getData()`
-   * @param forced `true` when performing a full re-render; `false` when performing a partial re-render
-   * @param hookName this hook's registered name
+   * The actor sheet has rendered all content and registered custom content. Is called on partial and full renders.
+   * @param {any} app The sheet application instance.
+   * @param {HTMLElement} element The sheet's HTML element.
+   * @param {CharacterSheetContext | NpcSheetContext | VehicleSheetContext} data The data context from `getData()`.
+   * @param {boolean} forced `true` when performing a full re-render; `false` when performing a partial re-render.
    *
    * @example
    * ```js
@@ -436,11 +411,10 @@ export class TidyHooks {
 
   /**
    * The item sheet has rendered all content and registered custom content. Is called on partial and full renders.
-   * @param app the target item sheet application class instance
-   * @param element the item sheet's HTML element
-   * @param data the data context from `getData()`
-   * @param forced `true` when performing a full re-render; `false` when performing a partial re-render
-   * @param hookName this hook's registered name
+   * @param {any} app The target item sheet application class instance.
+   * @param {HTMLElement} element The item sheet's HTML element.
+   * @param {ContainerSheetContext | ItemSheetContext} data The data context from `getData()`.
+   * @param {boolean} forced `true` when performing a full re-render; `false` when performing a partial re-render.
    *
    * @example
    * ```js
@@ -460,10 +434,9 @@ export class TidyHooks {
 
   /**
    * A tab has been selected on the sheet.
-   * @param app the sheet application instances
-   * @param element the sheet's HTML element
-   * @param newTabId the selected tab ID
-   * @param hookName this hook's registered name
+   * @param {any} app The sheet application instance.
+   * @param {HTMLElement} element The sheet's HTML element.
+   * @param {string} newTabId The selected tab ID.
    *
    * @example
    * ```js
