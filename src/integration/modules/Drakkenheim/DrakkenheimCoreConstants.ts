59991b8c3e679fdec40d745186c593a0b0ed7589
Kevin Garner <kevinleegarner@gmail.com>
2024-08-05 23:36:15 -0500

    Added base integration for Dungeons of Drakkenheim.

---
+++ src/integration/modules/Drakkenheim/DrakkenheimCoreConstants.ts
@@ -0,0 +1,4 @@
+export const DRAKKENHEIM_CORE_CONSTANTS = {
+    MODULE_ID: 'drakkenheim-core',
+    CONTAMINATION_LEVEL_FLAG_PROP: `flags.drakkenheim.contamination`,
+  } as const;
---
+++ src/integration/modules/Drakkenheim/DrakkenheimCoreContaminationTab.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import Pips from 'src/components/pips/Pips.svelte';
+    import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+    import type { ActorSheetContext } from 'src/types/types';
+    import { getContext } from 'svelte';
+    import type { Readable } from 'svelte/store';
+    import { DRAKKENHEIM_CORE_CONSTANTS } from './DrakkenheimCoreConstants';
+    const context = getContext<Readable<ActorSheetContext>>('context');
+    $: contanimationLevel =
+      FoundryAdapter.getProperty<number | undefined>(
+        $context.actor,
+        DRAKKENHEIM_CORE_CONSTANTS.CONTAMINATION_LEVEL_FLAG_PROP,
+      ) ?? 0;
+    $: levels = Array.fromRange(6, 1);
+    async function onContaminationLevelChanged(level: number): Promise<void> {
+      await $context.actor.update({
+        [DRAKKENHEIM_CORE_CONSTANTS.CONTAMINATION_LEVEL_FLAG_PROP]: level,
+      });
+    }
+    const localize = FoundryAdapter.localize;
+  </script>
+  
+  <div class="scroll-container">
+    <div class="description">
+      {localize('DRAKKENHEIM.ContaminationTable')}
+    </div>
+    <table class="contamination-table">
+      <thead>
+        <tr>
+          <th class="symptom">{localize('DRAKKENHEIM.ContaminationSymptoms')}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each levels as level}
+          <tr class:active={level <= contanimationLevel}>
+            <td class="symptom">
+              {localize(`DRAKKENHEIM.ContaminationEffect${level}`)}
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  
+    <Pips
+      total={levels.length}
+      selected={contanimationLevel}
+      on:change={(ev) => onContaminationLevelChanged(ev.detail)}
+    />
+  </div>
