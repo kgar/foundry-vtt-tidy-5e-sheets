f21a77dfe86d6e4e1b3dd9409683059fd0b7c3af
Kevin Lee Garner <kgar@users.noreply.github.com>
2024-08-06 21:27:39 -0500

    Added base integration for Dungeons of Drakkenheim. (#703)

---
+++ src/integration/modules/Drakkenheim/Pips.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  export let total = 5;
+  export let selected = 0;
+  const dispatcher = createEventDispatcher<{ change: number }>();
+  function onPipClick(index: number) {
+    if (selected === index + 1) {
+      dispatcher('change', selected - 1);
+      return;
+    }
+    dispatcher('change', index + 1);
+  }
+  $: pipArray = Array(total);
+</script>
+
+<div class="pips">
+  {#each pipArray as _, i}
+    <button
+      type="button"
+      class="pip"
+      class:inactive={selected < i + 1}
+      class:active={selected >= i + 1}
+      class:last-selected={selected === i + 1}
+      on:click={() => onPipClick(i)}><i></i></button
+    >
+  {/each}
+
+  <span aria-hidden="true" style="display: none" class="pip-end"></span>
+</div>
