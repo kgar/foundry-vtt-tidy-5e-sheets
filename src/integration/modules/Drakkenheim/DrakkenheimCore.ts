567601bd396d8bc9d719e8c3f9781fcf2f685c54
Kevin Lee Garner <kgar@users.noreply.github.com>
2024-08-06 21:02:52 -0500

    [#504] compat: Tidy x Dungeons of Drakkenheim dnd5e 3.2.x (#700)
    * Added base integration for Dungeons of Drakkenheim.
    
    * Added fix for data-tidy-theme-type attribute.
    
    * Tested and applied fixes specific to backporting.

---
+++ src/integration/modules/Drakkenheim/DrakkenheimCore.ts
@@ -0,0 +1,26 @@
+import type { Tidy5eSheetsApi } from 'src/api';
+import type { ModuleIntegrationBase } from 'src/integration/ModuleIntegrationBase';
+import DrakkenheimCoreContaminationTab from './DrakkenheimCoreContaminationTab.svelte';
+import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+
+export class DrakkenheimCoreModuleIntegration implements ModuleIntegrationBase {
+  get moduleId(): string {
+    return 'drakkenheim-core';
+  }
+  init(api: Tidy5eSheetsApi): void {
+    import('./pips.scss');
+    import('./DrakkenheimContaminationTab.scss');
+
+    const contaminationTab = new api.models.SvelteTab({
+      title: () => FoundryAdapter.localize('DRAKKENHEIM.Contamination'),
+      tabId: 'drakkenheim-contamination-tab',
+      component: DrakkenheimCoreContaminationTab,
+      getContext(context) {
+        return context;
+      },
+    });
+
+    api.registerCharacterTab(contaminationTab, { layout: 'classic' });
+    api.registerNpcTab(contaminationTab, { layout: 'classic' });
+  }
+}
