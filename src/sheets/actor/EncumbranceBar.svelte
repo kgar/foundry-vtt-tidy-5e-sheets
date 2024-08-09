a7fdc18aa496d9c48f34bb413cda3b379de9935e
Kevin Lee Garner <kgar@users.noreply.github.com>
2024-08-08 21:32:55 -0500

    Added support for visualizing the actual encumbrance stops instead of hardcoded 1/3 and 2/3 locations. (#707)

--- src/sheets/actor/EncumbranceBar.svelte
+++ src/sheets/actor/EncumbranceBar.svelte
@@ -16,15 +16,19 @@
   class="encumbrance"
   class:encumbered={$context.encumbrance.encumbered}
   title={localize('TIDY5E.Encumbrance')}
+  style="
+    --encumbrance-low: {$context.encumbrance.stops.encumbered}%; 
+    --encumbrance-high: {$context.encumbrance.stops.heavilyEncumbered}%
+  "
 >
   <span class="encumbrance-bar" style="width:{$context.encumbrance.pct}%" />
   <span class="encumbrance-label"
     >{$context.encumbrance.value} / {$context.encumbrance.max}</span
   >
-  <i class="encumbrance-breakpoint encumbrance-33 arrow-up" />
-  <i class="encumbrance-breakpoint encumbrance-33 arrow-down" />
-  <i class="encumbrance-breakpoint encumbrance-66 arrow-up" />
-  <i class="encumbrance-breakpoint encumbrance-66 arrow-down" />
+  <i class="encumbrance-breakpoint encumbrance-low arrow-up" />
+  <i class="encumbrance-breakpoint encumbrance-low arrow-down" />
+  <i class="encumbrance-breakpoint encumbrance-high arrow-up" />
+  <i class="encumbrance-breakpoint encumbrance-high arrow-down" />
 </div>
 
 <style lang="scss">
@@ -64,12 +68,12 @@
       border: 0.25rem solid transparent;
     }
 
-    .encumbrance-33 {
-      left: calc(100% / 3);
+    .encumbrance-low {
+      inset-inline-start: var(--encumbrance-low, 33%);
     }
 
-    .encumbrance-66 {
-      left: calc((100% / 3) * 2);
+    .encumbrance-high {
+      inset-inline-start: var(--encumbrance-high, 66%);
     }
 
     .arrow-up {
