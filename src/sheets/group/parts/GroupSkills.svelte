5e3a60c460b7d070bd638914f0e0df2481c5874f
Kevin Garner <kevinleegarner@gmail.com>
2024-09-07 13:54:00 -0500

    Plugged in group skill tooltip to group skills component.

--- src/sheets/group/parts/GroupSkills.svelte
+++ src/sheets/group/parts/GroupSkills.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import { CONSTANTS } from 'src/constants';
-  import type { GroupSheetClassicContext } from 'src/types/group.types';
+  import GroupSkillTooltip from 'src/tooltips/GroupSkillTooltip.svelte';
+  import { Tooltip } from 'src/tooltips/Tooltip';
+  import type {
+    GroupSheetClassicContext,
+    GroupSkill,
+  } from 'src/types/group.types';
   import { formatAsModifier } from 'src/utils/formatting';
   import { getContext } from 'svelte';
   import { type Readable } from 'svelte/store';
@@ -8,16 +13,45 @@
   let context = getContext<Readable<GroupSheetClassicContext>>(
     CONSTANTS.SVELTE_CONTEXT.CONTEXT,
   );
+
+  let groupSkillTooltip: GroupSkillTooltip;
+  let hoveredSkill: GroupSkill = {
+    key: '',
+    label: '',
+    members: [],
+    mod: Number.NEGATIVE_INFINITY,
+  };
+
+  function showGroupLanguageTooltip(
+    event: MouseEvent & { currentTarget: EventTarget & HTMLElement },
+    groupSkill: GroupSkill,
+  ): any {
+    if (!groupSkill.members.length) {
+      return;
+    }
+
+    hoveredSkill = groupSkill;
+
+    const target = event?.currentTarget;
+    setTimeout(() => {
+      Tooltip.show(target, groupSkillTooltip.getMarkup());
+    });
+  }
 </script>
 
 <div class="flex-row extra-small-gap flex-wrap">
   {#each $context.groupSkills as groupSkill}
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <!-- svelte-ignore a11y-mouse-events-have-key-events -->
     <span
-      title="TODO: List members, scores, mods, passives, and proficiencies in fancy tooltip"
       class="tag"
+      on:mouseover={(ev) => showGroupLanguageTooltip(ev, groupSkill)}
     >
       {groupSkill.label}
       {formatAsModifier(groupSkill.mod)}
     </span>
   {/each}
+</div>
+<div class="hidden">
+  <GroupSkillTooltip bind:this={groupSkillTooltip} skill={hoveredSkill} />
 </div>
