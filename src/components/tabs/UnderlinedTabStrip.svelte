1bc874693bc371b1259057a76e960fc1af9c8c13
Kevin Garner <kevinleegarner@gmail.com>
2024-09-07 01:09:32 -0500

    Made some refinements to the underlined tab strip.

--- src/components/tabs/UnderlinedTabStrip.svelte
+++ src/components/tabs/UnderlinedTabStrip.svelte
@@ -3,7 +3,7 @@
   export let selected: string;
 </script>
 
-<div class="underlined-tabs flex-row no-gap">
+<div class="underlined-tabs flex-row no-gap {$$restProps.class ?? ''}">
   {#each tabs as tab (tab)}
     <button
       type="button"
@@ -22,16 +22,15 @@
       width: auto;
       flex: 0 0 max-content;
       background: transparent;
-      line-height: 1rem;
       border: none;
       border-bottom: 0.1875rem solid transparent;
       color: var(--t5e-secondary-font-color);
-      padding: 0.5rem 1rem;
+      padding: 0.325rem 0.5rem;
       border-radius: 0;
       margin: 0;
 
       &.active {
-        border-bottom-color: var(--t5e-secondary-color);
+        border-bottom-color: var(--t5e-tertiary-color);
         color: var(--t5e-primary-font-color);
         font-weight: 500;
       }
