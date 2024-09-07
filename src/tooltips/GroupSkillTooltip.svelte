04d1ac3087f04f3346fca5ae1bfd531c14688512
Kevin Garner <kevinleegarner@gmail.com>
2024-09-07 13:53:09 -0500

    Added group skill tooltip and associated styles.

---
+++ src/tooltips/GroupSkillTooltip.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+  import type { GroupSkill } from 'src/types/group.types';
+  import { formatAsModifier } from 'src/utils/formatting';
+
+  export let skill: GroupSkill;
+
+  let tooltip: HTMLElement;
+
+  export function getMarkup() {
+    return tooltip.outerHTML;
+  }
+</script>
+
+<div bind:this={tooltip} class="document-list-summary-tooltip">
+  <h3>{skill.label}</h3>
+  <hr />
+  <ul>
+    {#each skill.members as member}
+      <li class="group-skill-grid">
+        <div
+          class="item-image"
+          style="background-image: url('{member.img}')"
+        ></div>
+        <div class="item-name truncate">{member.name}</div>
+        <div class="text-align-center">
+          {formatAsModifier(member.system.skills[skill.key]?.mod)}
+        </div>
+        <div class="text-align-center">
+          ({member.system.skills[skill.key]?.passive})
+        </div>
+        <div class="text-align-center">
+          <i
+            class="{FoundryAdapter.getProficiencyIconClass(
+              member.system.skills[skill.key]?.proficient,
+            )} fa-fw"
+            style="font-size: 12px;"
+          ></i>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</div>
