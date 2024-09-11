829aa8dda32f5e9864e2c2322bf69502b0da0897
Kevin Garner <kevinleegarner@gmail.com>
2024-09-10 23:43:36 -0500

    Extracted scoped SCSS to dedicated file and imported into core v2.

--- src/components/tabs/UnderlinedTabStrip.svelte
+++ src/components/tabs/UnderlinedTabStrip.svelte
@@ -5,7 +5,7 @@
   export let selected: string;
 </script>
 
-<div class="underlined-tabs flex-row no-gap {$$restProps.class ?? ''}">
+<div class="underlined-tab-strip flex-row no-gap {$$restProps.class ?? ''}">
   {#each tabs as tab (tab)}
     <button
       type="button"
@@ -18,29 +18,3 @@
     </button>
   {/each}
 </div>
-
-<style lang="scss">
-  .underlined-tabs {
-    .underlined-tab {
-      width: auto;
-      flex: 0 0 max-content;
-      background: transparent;
-      border: none;
-      border-bottom: 0.1875rem solid transparent;
-      color: var(--t5e-secondary-font-color);
-      padding: 0.325rem 0.5rem;
-      border-radius: 0;
-      margin: 0;
-
-      &:hover {
-        border-bottom-color: var(--t5e-faint-color);
-      }
-
-      &.active {
-        border-bottom-color: var(--t5e-tertiary-color);
-        color: var(--t5e-primary-font-color);
-        font-weight: 500;
-      }
-    }
-  }
-</style>
