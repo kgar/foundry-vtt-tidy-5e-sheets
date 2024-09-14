2d1f0e6a6001d7355539d6895cea7cbc80d2e802
Kevin Garner <kevinleegarner@gmail.com>
2024-09-14 01:22:13 -0500

    Resolved: - [x] NPC   - [x] source moved from `system.details.source` to `system.source` - [x] Vehicle   - [x] source moved from `system.details.source` to `system.source`

--- src/sheets/npc/NpcSheetFull.svelte
+++ src/sheets/npc/NpcSheetFull.svelte
@@ -150,7 +150,7 @@
 
               <InlineSource
                 document={$context.actor}
-                keyPath="system.details.source"
+                keyPath="system.source"
                 editable={$context.unlocked}
               />
             </DelimitedTruncatedContent>
