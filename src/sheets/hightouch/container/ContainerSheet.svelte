266c9d5590a8e99eb416a365767b82fa4ff4dd9d
Kevin Garner <kevinleegarner@gmail.com>
2024-09-28 11:54:20 -0500

    Added some App V2 stylings for all hightouch apps.
    Began laying out the item/container sheet.

--- src/sheets/hightouch/container/ContainerSheet.svelte
+++ src/sheets/hightouch/container/ContainerSheet.svelte
@@ -1,1 +1,6 @@
-🧹 Hello, Tidy 🏄‍♂️
+<aside class="sidebar">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere eius quis
+  omnis esse libero consectetur officiis repudiandae quisquam culpa aspernatur
+  nemo, rerum laudantium quae voluptates dolore doloribus corrupti amet modi.
+</aside>
+<main class="item-content">main content</main>
