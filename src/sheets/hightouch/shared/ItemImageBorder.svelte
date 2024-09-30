1d62686ac5d57761a75543613c09a9d6000c43d7
Kevin Garner <kevinleegarner@gmail.com>
2024-09-30 15:03:08 -0500

    Fixed issue where item image border doesn't scale when Foundry font size is increased.

--- src/sheets/hightouch/shared/ItemImageBorder.svelte
+++ src/sheets/hightouch/shared/ItemImageBorder.svelte
@@ -1,9 +1,8 @@
 <div class="item-image-border">
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="112"
-    height="107"
     viewBox="0 0 112 107"
+    preserveAspectRatio="xMidYMid meet"
     fill="none"
   >
     <path
