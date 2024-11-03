c673955f53fa99cf6755865f70f79c5470604b40
Kevin Garner <kevinleegarner@gmail.com>
2024-11-03 15:18:50 -0600

    Added item descriptions to item descriptions tab.

--- src/sheets/hightouch/item/tabs/ItemDescriptionsTab.svelte
+++ src/sheets/hightouch/item/tabs/ItemDescriptionsTab.svelte
@@ -1,1 +1,16 @@
-Hello Item Description Tab! 👋
+<script lang="ts">
+  import { getContext } from 'svelte';
+  import ItemDescriptions from '../../shared/ItemDescriptions.svelte';
+  import type { Readable } from 'svelte/store';
+  import { CONSTANTS } from 'src/constants';
+  import type { ContainerSheetHightouchContext } from 'src/types/item.types';
+
+  let context = getContext<Readable<ContainerSheetHightouchContext>>(
+    CONSTANTS.SVELTE_CONTEXT.CONTEXT,
+  );
+</script>
+
+<ItemDescriptions
+  document={$context.document}
+  itemDescriptions={$context.itemDescriptions}
+/>
