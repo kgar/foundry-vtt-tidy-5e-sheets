911e79fc4fc5b7b84bc06e9f757019474b56fe17
Kevin Garner <kevinleegarner@gmail.com>
2024-11-04 18:07:35 -0600

    Began implementing container currency tracker.

--- src/sheets/hightouch/container/parts/ContainerCurrency.svelte
+++ src/sheets/hightouch/container/parts/ContainerCurrency.svelte
@@ -1,1 +1,47 @@
-💵💵 Hello, container currency 💵💵
+<script lang="ts">
+  import TextInput from 'src/components/inputs/TextInput.svelte';
+  import { CONSTANTS } from 'src/constants';
+  import type { ContainerSheetHightouchContext } from 'src/types/item.types';
+  import { getContext } from 'svelte';
+  import type { Readable } from 'svelte/store';
+
+  let context = getContext<Readable<ContainerSheetHightouchContext>>(
+    CONSTANTS.SVELTE_CONTEXT.CONTEXT,
+  );
+
+  $: currencies = Object.keys(CONFIG.DND5E.currencies).map((key) => ({
+    key: key,
+    value: $context.system.currency[key] as number,
+  }));
+
+  function abbreviateCurrency(currencyKey: string) {
+    return (
+      CONFIG.DND5E.currencies[
+        currencyKey as keyof typeof CONFIG.DND5E.currencies
+      ]?.abbreviation ?? currencyKey
+    );
+  }
+</script>
+
+<section class="currency-tracker">
+  {#each currencies as currency}
+    <label class="input-group">
+      <TextInput
+        document={$context.document}
+        field="system.currency.{currency.key}"
+        id="{$context.document.id}-system.currency.{currency.key}"
+        value={currency.value}
+        allowDeltaChanges={true}
+        selectOnFocus={true}
+        disabled={!$context.editable || $context.lockMoneyChanges}
+        class="currency-item currency-{currency.key}"
+        placeholder="0"
+      />
+      <span class="denomination {currency.key}" data-denom={currency.key}>
+        {abbreviateCurrency(currency.key)}
+      </span>
+    </label>
+  {/each}
+  <!-- CURRENCY CONVERT Button? -->
+  <!-- ITEM ADD Button -->
+</section>
