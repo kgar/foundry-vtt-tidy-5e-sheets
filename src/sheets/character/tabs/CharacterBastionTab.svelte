8aa8524f77f8731e53e3d7f090506bfe726c8717
Kevin Garner <kevinleegarner@gmail.com>
2024-11-16 08:35:31 -0600

    Added some demo HTML in preparation to style.

--- src/sheets/character/tabs/CharacterBastionTab.svelte
+++ src/sheets/character/tabs/CharacterBastionTab.svelte
@@ -1,1 +1,158 @@
-🏰 Hello bastions! 🏯
+<section class="name">
+  <div class="document-name">Bastion Name Here</div>
+  <!-- TODO: Toggle readonly name with editable input on locked -->
+</section>
+<section class="contents">
+  <section class="facilities special">
+    <h3>
+      <i class="fas fa-building-columns"></i> Special Facilities (LOCALIZE)
+    </h3>
+    <ul>
+      <li class="facility special">
+        <div class="facility-header">
+          <a class="title"> Barrack </a>
+          <span class="subtitle"> Recruit &bull; Roomy &bull; Level 5 </span>
+        </div>
+        <span class="sub-header"> Hirelings </span>
+        <div class="slots hirelings">
+          <div
+            data-actor-uuid="Actor.q2zeMwiDSCq2rcso"
+            class="slot occupant-slot hireling"
+            data-index="0"
+          >
+            <img
+              src="systems/dnd5e/tokens/humanoid/Hobgoblin.webp"
+              alt="Hobgoblin (Tidy)"
+            />
+          </div>
+          <div
+            data-actor-uuid="Actor.q2zeMwiDSCq2rcso"
+            class="slot occupant-slot hireling"
+            data-index="1"
+          >
+            <img
+              src="systems/dnd5e/tokens/humanoid/Hobgoblin.webp"
+              alt="Hobgoblin (Tidy)"
+            />
+          </div>
+          <div
+            data-actor-uuid="Actor.q2zeMwiDSCq2rcso"
+            class="slot occupant-slot hireling"
+            data-index="2"
+          >
+            <img
+              src="systems/dnd5e/tokens/humanoid/Hobgoblin.webp"
+              alt="Hobgoblin (Tidy)"
+            />
+          </div>
+          <div class="slot occupant-slot hireling empty" data-index="3"></div>
+        </div>
+        <span class="sub-header"> Defenders </span>
+        <div class="slots defenders">
+          <div
+            data-actor-uuid="Actor.q2zeMwiDSCq2rcso"
+            class="slot occupant-slot defender"
+            data-index="0"
+          >
+            <img
+              src="systems/dnd5e/tokens/humanoid/Hobgoblin.webp"
+              alt="Hobgoblin (Tidy)"
+            />
+          </div>
+          <div
+            data-actor-uuid="Actor.q2zeMwiDSCq2rcso"
+            class="slot occupant-slot defender"
+            data-index="1"
+          >
+            <img
+              src="systems/dnd5e/tokens/humanoid/Hobgoblin.webp"
+              alt="Hobgoblin (Tidy)"
+            />
+          </div>
+          <div
+            data-actor-uuid="Actor.q2zeMwiDSCq2rcso"
+            class="slot occupant-slot defender"
+            data-index="2"
+          >
+            <img
+              src="systems/dnd5e/tokens/humanoid/Hobgoblin.webp"
+              alt="Hobgoblin (Tidy)"
+            />
+          </div>
+          <div class="slot occupant-slot defender empty" data-index="3"></div>
+        </div>
+        <span class="sub-header"> Order </span>
+        <div class="meter progress">
+          <span class="order"> Recruit </span>
+          <span>
+            <span class="elapsed">45</span> / <span class="total">80</span>
+          </span>
+        </div>
+      </li>
+      <li class="facility special"></li>
+      <li class="facility empty"></li>
+    </ul>
+  </section>
+  <section class="facilities basic">
+    <h3>
+      <i class="fas fa-chess-rook"></i> Basic Facilities (LOCALIZE)
+    </h3>
+    <ul>
+      <li class="facility">
+        <div class="facility-header">
+          <a class="title">Keeping Room</a>
+          <ul>
+            <li>Unbuilt</li>
+          </ul>
+        </div>
+      </li>
+      <li class="facility">
+        <div class="facility-header">
+          <a class="title">Bedchambers</a>
+          <ul>
+            <li>Cramped</li>
+            <li>Bedroom</li>
+          </ul>
+          <a class="transparent-icon-button">
+            <i class="fas fa-ellipsis-vertical"></i>
+          </a>
+        </div>
+        <span class="sub-header"> Order </span>
+        <div class="meter progress">
+          <span class="order"> Enlarge </span>
+          <span>
+            <span class="elapsed">45</span> / <span class="total">80</span>
+          </span>
+        </div>
+      </li>
+      <li class="facility empty">Build Facility</li>
+    </ul>
+  </section>
+  <section class="roster defenders">
+    <h3>Defenders (LOCALIZE)</h3>
+    <ul>
+      <li>Actor Image</li>
+      <li>Actor Image</li>
+      <li>Actor Image</li>
+      <li>Actor Image</li>
+    </ul>
+
+    <ul>
+      <li class="empty">Bastion is undefended. (LOCALIZE)</li>
+    </ul>
+  </section>
+  <section class="roster hirelings">
+    <h3>Hirelings (LOCALIZE)</h3>
+    <ul>
+      <li>Actor Image</li>
+      <li>Actor Image</li>
+      <li>Actor Image</li>
+      <li>Actor Image</li>
+    </ul>
+
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</section>
+<section class="description"></section>
