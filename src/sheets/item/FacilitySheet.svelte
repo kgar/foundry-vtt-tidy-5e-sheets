0d52356aa1c09f118c293e3ae4393dd9d27e4a9d
Kevin Garner <kevinleegarner@gmail.com>
2024-11-24 11:42:09 -0600

    Added baseline scaffolding for Facility item sheet.

---
+++ src/sheets/item/FacilitySheet.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import { getContext } from 'svelte';
+    import type { Readable } from 'svelte/store';
+    import type { ItemSheetContext } from 'src/types/item.types';
+    import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+    import Tabs from 'src/components/tabs/Tabs.svelte';
+    import TabContents from 'src/components/tabs/TabContents.svelte';
+    import TextInput from 'src/components/inputs/TextInput.svelte';
+    import ItemProfilePicture from './parts/ItemProfilePicture.svelte';
+    import Source from '../shared/Source.svelte';
+    import { CONSTANTS } from 'src/constants';
+  
+    let context = getContext<Readable<ItemSheetContext>>(
+      CONSTANTS.SVELTE_CONTEXT.CONTEXT,
+    );
+  
+    $: appId = $context.document.id;
+  
+    let selectedTabId: string;
+  
+    const localize = FoundryAdapter.localize;
+  </script>
+
+<header class="sheet-header flexrow gap">
+    <ItemProfilePicture />
+    <div
+      class="header-details flexrow small-gap"
+      data-tidy-sheet-part={CONSTANTS.SHEET_PARTS.NAME_HEADER_ROW}
+    >
+      <h1
+        class="charname"
+        data-tidy-sheet-part={CONSTANTS.SHEET_PARTS.NAME_CONTAINER}
+      >
+        <TextInput
+          id="{appId}-name"
+          field="name"
+          document={$context.item}
+          value={$context.item.name}
+          attributes={{ 'data-tidy-item-name': $context.item.name }}
+          disabled={!$context.editable}
+          title={$context.item.name}
+        />
+      </h1>
+  
+      <div class="item-subtitle">
+        <h4 class="item-type">{$context.itemType ?? ''}</h4>
+        <span class="item-status">{$context.itemStatus ?? ''}</span>
+      </div>
+  
+      <ul class="summary flexrow">
+        <li class="flex-row">
+          <Source
+            document={$context.item}
+            keyPath="system.source"
+            editable={$context.editable}
+          />
+        </li>
+      </ul>
+    </div>
+  </header>
+  <Tabs bind:selectedTabId tabs={$context.tabs} />
+  <section class="tidy-sheet-body">
+    <TabContents tabs={$context.tabs} {selectedTabId} />
+  </section>
+  
