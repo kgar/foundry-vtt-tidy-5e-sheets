4f9542bfb19d212a581efb37a276b66e5df02f0f
Kevin Garner <kevinleegarner@gmail.com>
2024-12-10 10:41:50 -0600

    Converted inline toggle service to use reactive maps and sets.

--- src/features/expand-collapse/InlineToggleService.svelte.ts
+++ src/features/expand-collapse/InlineToggleService.svelte.ts
@@ -1,25 +1,24 @@
-// kgar-migration-task - consider a SvelteSet for this one.
+import { SvelteMap, SvelteSet } from 'svelte/reactivity';
+
 /** Service for tracking which inline entities are currently expanded to show their contents. */
 export class InlineToggleService {
-  /** Entity IDs which represent all entities that are currently expanded. */
-  map: Map<string, Set<string>> = $state(new Map<string, Set<string>>());
+  /** A map of tab IDs to set of entity IDs, typically Items. */
+  map = new SvelteMap<string, SvelteSet<string>>();
 
   /** Toggle an entity to be expanded or collapsed, based on its current state. */
   toggle(tabId: string, entityId: string) {
-    const tabs = this.map;
+    // const tabs = this.map;
 
-    if (!tabs.has(tabId)) {
-      tabs.set(tabId, new Set<string>());
+    if (!this.map.has(tabId)) {
+      this.map.set(tabId, new SvelteSet<string>());
     }
 
-    const tab = tabs.get(tabId)!;
+    const tab = this.map.get(tabId)!;
 
     if (tab.has(entityId)) {
       tab.delete(entityId);
     } else {
       tab.add(entityId);
     }
-
-    this.map = tabs;
   }
 }
