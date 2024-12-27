7f3ea3d163d4c985643814a33565a11ea951ed94
Kevin Garner <kevinleegarner@gmail.com>
2024-12-26 22:31:44 -0600

    npx npm-check-updates -u vite @sveltejs/vite-plugin-svelte and moved to vite 6
    npm uninstall terser
    
    npm update
    
    Updated vite config to use esbuild and to preserve identifiers and keep names.
    
    Updated script and stylesheet names to match new build setup.

--- prepare-dist-for-dev.js
+++ prepare-dist-for-dev.js
@@ -26,13 +26,13 @@ function prepareDistForDev() {
   // Copy all of the public folder contents to dist
   fs.cpSync(publicFolder, distFolder, { recursive: true });
 
-  // Put empty main.js and style.css files in place
+  // Put empty tidy5e-sheet.js and tidy5e-sheet.css files in place
   const message =
     'Hello, Tidy 5e dev 👋. This file exists so that foundry is happy and will consider the module eligible to load up. The vite server proxy will do the rest.';
 
-  fs.writeFileSync(path.join(distFolder, 'main.js'), `// ${message}`);
+  fs.writeFileSync(path.join(distFolder, 'tidy5e-sheet.js'), `// ${message}`);
 
-  fs.writeFileSync(path.join(distFolder, 'style.css'), `/* ${message} */`);
+  fs.writeFileSync(path.join(distFolder, 'tidy5e-sheet.css'), `/* ${message} */`);
   
   fs.writeFileSync(path.join(distFolder, 'tidy5e-sheet.lock'), "🔒");
 }
