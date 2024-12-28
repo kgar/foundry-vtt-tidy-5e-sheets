f0342a3787fd028767ffaee9bb964ad2c2ff4ee9
Kevin Garner <kevinleegarner@gmail.com>
2024-12-28 15:25:51 -0600

    Ported embedded document change handling from App V2 Svelte Mixin to the v1 custom content renderer as a stopgap until the App V2 migration completes in its entirety.

--- src/sheets/CustomContentRenderer.ts
+++ src/sheets/CustomContentRenderer.ts
@@ -7,6 +7,7 @@ import {
   wrapCustomHtmlForRendering,
 } from 'src/utils/content';
 import { isNil } from 'src/utils/data';
+import { processInputChangeDelta } from 'src/utils/form';
 import { debug, error, warn } from 'src/utils/logging';
 
 type CustomContentRenderParams = {
@@ -107,7 +108,7 @@ export class CustomContentRenderer {
           (isFullRender || tab.content.renderScheme === 'handlebars')
         ) {
           tabEl.innerHTML = tab.content.html;
-        } 
+        }
 
         if (tab.onRender) {
           tab.onRender({
@@ -223,5 +224,77 @@ export class CustomContentRenderer {
       .each((_: number, el: HTMLElement) => {
         superActivateListeners.call(sheet, globalThis.$(el));
       });
+
+    html
+      .find('input[data-name], textarea[data-name], select[data-name]')
+      .off('change.embedded-doc-compatiblity-event-listeners')
+      .on(
+        'change.embedded-doc-compatiblity-event-listeners',
+        function (event: any) {
+          // TODO: is jquery giving me the raw event?
+          _submitEmbeddedDocumentChange(sheet.document, event);
+        }
+      );
   }
+}
+
+/*----------------------------------------------*/ 
+/* Ported from App V2 Svelte Mixin as a stopgap */
+/*----------------------------------------------*/
+async function _submitEmbeddedDocumentChange(
+  foundryDocument: any,
+  event: InputEvent & { target: HTMLInputElement }
+) {
+  const itemId =
+    event.target.closest<HTMLElement>('[data-item-id]')?.dataset.itemId;
+  if (itemId) {
+    await _submitEmbeddedItemChange(foundryDocument, event, itemId);
+  }
+}
+
+async function _submitEmbeddedItemChange(
+  foundryDocument: any,
+  event: InputEvent & { target: HTMLInputElement },
+  itemId: string
+) {
+  event.stopImmediatePropagation();
+
+  const item = await getItem(foundryDocument, itemId);
+  const field = event.target.getAttribute('data-name')!;
+
+  let valueToSave: string | number = event.target.value;
+
+  // For deltas, parse the resulting delta value
+  if (event.target.matches('[inputmode="numeric"]')) {
+    valueToSave = processInputChangeDelta(
+      event.target.value,
+      item,
+      field
+    )?.toString();
+  }
+
+  // For numeric changes, enforce min/max on the value to save
+  if (event.target.matches('[inputmode="numeric"], [type="number"]')) {
+    const minAttribute = event.target.getAttribute('min');
+    const min = !isNil(minAttribute, '') ? Number(minAttribute) : -Infinity;
+
+    const maxAttribute = event.target.getAttribute('max');
+    const max = !isNil(maxAttribute, '') ? Number(maxAttribute) : Infinity;
+
+    const valueAsNumber = Number(valueToSave);
+    valueToSave = Math.clamp(valueAsNumber, min, max);
+
+    if (item && !Number.isNaN(valueToSave)) {
+      event.target.value = valueToSave?.toString();
+    }
+  }
+
+  // Save the value to the document, whatever that value ultimately became
+  item.update({ [field]: valueToSave });
+}
+
+async function getItem(foundryDocument: any, id: string) {
+  if (foundryDocument.type === 'container')
+    return foundryDocument.system.getContainedItem(id);
+  return foundryDocument.items.get(id);
 }
