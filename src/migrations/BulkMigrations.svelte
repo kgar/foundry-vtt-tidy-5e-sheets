9511d76db6678f9b70599fdc240a13af5dbd0ac3
Kevin Garner <kevinleegarner@gmail.com>
2025-01-24 00:15:42 -0600

    Purged old migrations. Kept CCSS migration.
    New Migrations: Changed from a notification to a simple chat message. Updated migration notification so that it does not notify new Tidy GM users on first load. Instead, it quietly sets the tally and will notify the the next time there's a new migration.

--- src/migrations/BulkMigrations.svelte
+++ src/migrations/BulkMigrations.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
-  import NpcDeathSavesMigration from './v3/NpcDeathSavesMigration.svelte';
-  import CharacterBiographyMigration from './v2/CharacterBiographyMigration.svelte';
   import type { Tab } from 'src/types/types';
   import Tabs from 'src/components/tabs/Tabs.svelte';
   import TabContents from 'src/components/tabs/TabContents.svelte';
   import { CONSTANTS } from 'src/constants';
   import Notice from 'src/components/notice/Notice.svelte';
   import CcssToTidyMigration from './v3/CcssToTidyMigration.svelte';
-  import FavoritesToSystemMigration from './v4/FavoritesToSystemMigration.svelte';
-  import BondsIdealsFlawsToSystemMigration from './v5/BondsIdealsFlawsToSystemMigration.svelte';
-  import SpellClassToSourceClassMigration from './v5/SpellClassToSourceClassMigration.svelte';
-  import NpcExhaustionToSystemMigration from './v5/NpcExhaustionToSystemMigration.svelte';
 
   interface Props {
     selectedTabId?: string;
@@ -25,58 +19,10 @@
 
   const tabs: Tab[] = [
     {
-      id: CONSTANTS.TAB_MIGRATIONS_NPC_EXHAUSTION,
-      title: 'TIDY5E.Settings.Migrations.NpcExhaustion.sectionTitle',
-      content: {
-        component: NpcExhaustionToSystemMigration,
-        type: 'svelte',
-      },
-    },
-    {
-      id: CONSTANTS.TAB_MIGRATIONS_SPELL_CLASS_TO_SOURCE_CLASS,
-      title: 'TIDY5E.Settings.Migrations.SpellClassToSourceClass.sectionTitle',
-      content: {
-        component: SpellClassToSourceClassMigration,
-        type: 'svelte',
-      },
-    },
-    {
-      id: CONSTANTS.TAB_MIGRATIONS_BONDS_IDEALS_FLAWS_TO_SYSTEM,
-      title: 'TIDY5E.Settings.Migrations.BondsIdealsFlawsToSystem.sectionTitle',
-      content: {
-        component: BondsIdealsFlawsToSystemMigration,
-        type: 'svelte',
-      },
-    },
-    {
-      id: CONSTANTS.TAB_MIGRATIONS_FAVORITES_TO_SYSTEM,
-      title: 'TIDY5E.Settings.Migrations.FavoritesToSystem.sectionTitle',
-      content: {
-        component: FavoritesToSystemMigration,
-        type: 'svelte',
-      },
-    },
-    {
       id: CONSTANTS.TAB_MIGRATIONS_CCSS_TO_TIDY,
       title: 'TIDY5E.Settings.Migrations.CcssToTidy.sectionTitle',
       content: {
         component: CcssToTidyMigration,
-        type: 'svelte',
-      },
-    },
-    {
-      id: CONSTANTS.TAB_MIGRATIONS_NPC_DEATH,
-      title: 'TIDY5E.Settings.Migrations.NpcDeathSaves.sectionTitle',
-      content: {
-        component: NpcDeathSavesMigration,
-        type: 'svelte',
-      },
-    },
-    {
-      id: CONSTANTS.TAB_MIGRATIONS_CHARACTER_BIOGRAPHY,
-      title: 'TIDY5E.Settings.Migrations.CharacterBiography.sectionTitle',
-      content: {
-        component: CharacterBiographyMigration,
         type: 'svelte',
       },
     },
