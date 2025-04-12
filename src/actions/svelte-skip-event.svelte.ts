fd515c126b327866918c511b98dcf98103dade40
kgar <kevinleegarner@gmail.com>
2025-04-12 01:40:22 -0500

    Skipped svelte when clicking on any rollable block title component. This fix is to prevent svelte's assign `__root` HTML element prop from causing an initiative roll to error out from a token on the canvas with an unrolled initiative on the tracker. See https://discord.com/channels/915186263609454632/1180720388070711357/1360502900404654173

---
+++ src/actions/svelte-skip-event.svelte.ts
@@ -0,0 +1,34 @@
+import type { MouseEventHandler } from 'svelte/elements';
+
+type SkipSvelteOnClickArgs = {
+  handler?: MouseEventHandler<HTMLElement>;
+};
+
+export function skipSvelte_onclick(
+  node: HTMLElement,
+  args: SkipSvelteOnClickArgs
+) {
+  if (!args.handler) {
+    return;
+  }
+
+  $effect(() => {
+    const controller = new AbortController();
+
+    node.addEventListener(
+      'click',
+      (event) => {
+        args.handler?.(
+          event as MouseEvent & { currentTarget: EventTarget & HTMLElement }
+        );
+      },
+      {
+        signal: controller.signal,
+      }
+    );
+
+    return () => {
+      controller.abort();
+    };
+  });
+}
