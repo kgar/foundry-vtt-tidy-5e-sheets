2331340808aad9e59196f39cea81947498b3d543
kgar <kevinleegarner@gmail.com>
2025-04-16 21:18:50 -0500

    Just iterating on character sheet.

--- src/sheets/quadrone/actor/CharacterSheet.svelte
+++ src/sheets/quadrone/actor/CharacterSheet.svelte
@@ -11,18 +11,38 @@
   let selectedTabId: string = $state('');
 
   let sidebarExpanded = $state(false);
+
+  let hpValue = $derived(context.system.attributes?.hp?.value ?? 0);
+  let hpMax = $derived(context.system.attributes?.hp?.max ?? 0);
+  let hpPct = $derived(context.system.attributes?.hp?.pct ?? 0);
+  let hpTemp = $derived(context.system.attributes?.hp?.temp ?? 0);
+  let hpTempMax = $derived(context.system.attributes?.hp?.tempMax ?? 0);
 </script>
 
 <header class="sheet-header">
-  <div class="character-vitals">Character vitals here</div>
+  <div class="character-vitals-container">
+    <img class="character-image" src={context.actor.img} />
+    <div class="character-vitals">
+      <div
+        class="meter progress hit-points"
+        data-bar-severity="health"
+        style="--bar-percentage: {hpPct.toFixed(0)}%"
+      >
+        <label for=""></label>
+      </div>
+      Row: Hit Dice Bar, Exhaustion Tracker, Death Saves Toggle, Config button (?)<br
+      />
+      Row: (If toggled on) Death Saves Tracker<br />
+    </div>
+  </div>
   <div class="character-profile">
     <div class="character-name">Character name here</div>
     <div class="character-summary">
       Main movement, size, species, classes (level, main stat, DC)
     </div>
   </div>
-  <div class="sheet-header-buttons">Sheet header buttons</div>
-  <div class="level-badge-container">Level badge container</div>
+  <div class="sheet-header-actions">Sheet header buttons</div>
+  <div class="level-block"></div>
   <div class="abilities-container">AC, Abilities, Init, Concentration</div>
   <div class="tabs-row">
     <a
