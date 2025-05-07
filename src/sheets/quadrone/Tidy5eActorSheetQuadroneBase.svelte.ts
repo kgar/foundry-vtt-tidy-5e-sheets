f451cf25fa81af673f6875b6d098bc39c92f2343
kgar <kevinleegarner@gmail.com>
2025-05-06 22:37:58 -0500

    Added base actor drag and drop handling to new quadrone base class.

--- src/sheets/quadrone/Tidy5eActorSheetQuadroneBase.svelte.ts
+++ src/sheets/quadrone/Tidy5eActorSheetQuadroneBase.svelte.ts
@@ -1,11 +1,14 @@
 import { CONSTANTS } from 'src/constants';
 import { CoarseReactivityProvider } from 'src/features/reactivity/CoarseReactivityProvider.svelte';
 import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+import { TidyFlags } from 'src/foundry/TidyFlags';
+import type { DropEffectValue } from 'src/mixins/DragAndDropBaseMixin';
 import { SvelteApplicationMixin } from 'src/mixins/SvelteApplicationMixin.svelte';
 import {
   TidyExtensibleDocumentSheetMixin,
   type TidyDocumentSheetRenderOptions,
 } from 'src/mixins/TidyDocumentSheetMixin.svelte';
+import { settings } from 'src/settings/settings.svelte';
 import type { ApplicationConfiguration } from 'src/types/application.types';
 import type { Ability } from 'src/types/dnd5e.actor5e.types';
 import type { Item5e } from 'src/types/item.types';
@@ -20,7 +23,9 @@ import type {
 } from 'src/types/types';
 import { applyThemeToApplication } from 'src/utils/applications.svelte';
 import { splitSemicolons } from 'src/utils/array';
+import { isNil } from 'src/utils/data';
 import { debug, error } from 'src/utils/logging';
+import { firstOfSet } from 'src/utils/set';
 
 /*
   TODO: Some things to remember to implement:
@@ -548,6 +553,534 @@ export function Tidy5eActorSheetQuadroneBase<
         `;
       if (element.dataset.attribution)
         element.dataset.tooltipClass = 'property-attribution';
+    }
+
+    /* -------------------------------------------- */
+    /*  Drag and Drop
+        /* -------------------------------------------- */
+
+    _allowedDropBehaviors(event: DragEvent, data: any) {
+      if (!data.uuid) {
+        return new Set<DropEffectValue>(['copy', 'link']);
+      }
+
+      const allowed = new Set<DropEffectValue>(['copy', 'move', 'link']);
+      const s = foundry.utils.parseUuid(data.uuid);
+      const t = foundry.utils.parseUuid(this.document.uuid);
+      const sCompendium =
+        s.collection instanceof
+        foundry.documents.collections.CompendiumCollection;
+      const tCompendium =
+        t.collection instanceof
+        foundry.documents.collections.CompendiumCollection;
+
+      // If either source or target are within a compendium, but not inside the same compendium, move not allowed
+      if ((sCompendium || tCompendium) && s.collection !== t.collection) {
+        allowed.delete('move');
+      }
+
+      return allowed;
+    }
+
+    _defaultDropBehavior(event: DragEvent, data: any) {
+      if (!data.uuid) {
+        return 'copy';
+      }
+
+      const d = foundry.utils.parseUuid(data.uuid);
+      const t = foundry.utils.parseUuid(this.document.uuid);
+      const base = d.embedded?.length ? 'document' : 'primary';
+
+      return d.collection === t.collection &&
+        d[`${base}Id`] === t[`${base}Id`] &&
+        d[`${base}Type`] === t[`${base}Type`]
+        ? 'move'
+        : 'copy';
+    }
+
+    _onDragOver(event: DragEvent & { currentTarget: HTMLElement }) {
+      const data =
+        foundry.applications.ux.DragDrop.implementation.getPayload(event);
+
+      if (event.dataTransfer == null) {
+        return;
+      }
+
+      foundry.applications.ux.DragDrop.implementation.dropEffect =
+        event.dataTransfer.dropEffect =
+          foundry.utils.getType(data) === 'Object'
+            ? this._dropBehavior(event, data)
+            : 'copy';
+    }
+
+    /**
+     * The behavior for the dropped data. When called during the drop event, ensure this is called before awaiting
+     * anything or the drop behavior will be lost.
+     */
+    _dropBehavior(event: DragEvent, data: unknown): DropEffectValue {
+      const allowed = this._allowedDropBehaviors(event, data);
+
+      let behavior =
+        foundry.applications.ux.DragDrop.implementation.dropEffect ??
+        event.dataTransfer?.dropEffect;
+
+      if (event.type === 'dragover') {
+        if (dnd5e.utils.areKeysPressed(event, 'dragMove')) behavior = 'move';
+        else if (dnd5e.utils.areKeysPressed(event, 'dragCopy')) {
+          behavior = 'copy';
+        } else {
+          behavior = this._defaultDropBehavior(event, data);
+        }
+      }
+
+      if (behavior !== 'none' && !allowed.has(behavior)) {
+        return firstOfSet(allowed) ?? 'none';
+      }
+
+      return behavior || 'copy';
+    }
+
+    /** @inheritDoc */
+    _onDragStart(
+      event: DragEvent & { target: HTMLElement; currentTarget: HTMLElement }
+    ) {
+      const li = event.currentTarget;
+      if (event.target.classList.contains('content-link')) return;
+
+      if (li.dataset.effectId && li.dataset.parentId) {
+        const effect = this.actor.items
+          .get(li.dataset.parentId)
+          ?.effects.get(li.dataset.effectId);
+        if (effect)
+          event.dataTransfer?.setData(
+            'text/plain',
+            JSON.stringify(effect.toDragData())
+          );
+        return;
+      }
+
+      super._onDragStart(event);
+    }
+
+    async _onDrop(
+      event: DragEvent & { currentTarget: HTMLElement }
+    ): Promise<any> {
+      this._currentDragEvent = event;
+      const data = foundry.applications.ux.TextEditor.getDragEventData(event);
+      const actor = this.actor;
+      // TODO: Extract hook call
+      const allowed = Hooks.call('dropActorSheetData', actor, this, data);
+      if (allowed === false) return;
+
+      // Handle different data types
+      switch (data.type) {
+        case 'Actor':
+          return this._onDropActor(event, data);
+        case 'Item':
+          return this._onDropItem(event, data);
+        case 'Folder':
+          return this._onDropFolder(event, data);
+      }
+
+      return super._onDrop(event);
+    }
+
+    /** @override */
+    async _onDropActor(event: DragEvent, data: any) {
+      const droppedActor = await fromUuid(data.uuid);
+
+      const canPolymorph =
+        game.user.isGM ||
+        (this.actor.isOwner && game.settings.get('dnd5e', 'allowPolymorphing'));
+
+      // TODO: Make the character sheet handle bastion tab check. This is violating separation of concerns.
+      if (
+        !canPolymorph ||
+        this.currentTabId === CONSTANTS.TAB_CHARACTER_BASTION
+      ) {
+        return;
+      }
+
+      // Configure the transformation
+      const settings =
+        await dnd5e.applications.actor.TransformDialog.promptSettings(
+          this.actor,
+          droppedActor,
+          {
+            transform: {
+              settings: game.settings.get('dnd5e', 'transformationSettings'),
+            },
+          }
+        );
+
+      if (!settings) {
+        return;
+      }
+
+      await game.settings.set(
+        'dnd5e',
+        'transformationSettings',
+        settings.toObject()
+      );
+
+      return this.actor.transformInto(droppedActor, settings);
+    }
+
+    async _onDropItem(
+      event: DragEvent & { currentTarget: HTMLElement },
+      data: unknown
+    ): Promise<object | boolean | undefined> {
+      const behavior = this._dropBehavior(event, data);
+
+      if (!this.actor.isOwner || behavior === 'none') {
+        return false;
+      }
+
+      const item = await Item.implementation.fromDropData(data);
+
+      // Handle moving out of container & item sorting
+      if (behavior === 'move' && this.actor.uuid === item.parent?.uuid) {
+        const removingFromContainer = !isNil(item.system.container);
+        if (removingFromContainer) {
+          await item.update({ 'system.container': null });
+        }
+
+        const itemData = item.toObject();
+
+        const sourceSection = foundry.utils.getProperty(
+          itemData,
+          TidyFlags.section.prop
+        );
+
+        const targetSection = (event.target as HTMLElement | null)
+          ?.closest('[data-tidy-section-key][data-custom-section="true"]')
+          ?.getAttribute('data-tidy-section-key');
+
+        const isMovedToNewSection =
+          !isNil(targetSection?.trim(), '') && sourceSection !== targetSection;
+
+        const isMovedToDefaultSection =
+          !isNil(sourceSection?.trim(), '') && isNil(targetSection?.trim(), '');
+
+        const initialSortResult = await FoundryAdapter.onSortItemForActor(
+          this.actor,
+          event,
+          itemData
+        );
+
+        if (removingFromContainer) {
+          return initialSortResult;
+        }
+
+        if (isMovedToNewSection) {
+          TidyFlags.section.set(item, targetSection);
+          return;
+        } else if (isMovedToDefaultSection) {
+          TidyFlags.section.unset(item);
+          return;
+        }
+
+        return initialSortResult;
+      }
+
+      return this._onDropItemCreate(item, event, behavior);
+    }
+
+    /**
+     * Handle the final creation of dropped Item data on the Actor.
+     */
+    async _onDropItemCreate(
+      itemData: Item5e[] | Item5e,
+      event: DragEvent,
+      behavior?: DropEffectValue
+    ): Promise<Item5e[]> {
+      let items = itemData instanceof Array ? itemData : [itemData];
+      const itemsWithoutAdvancement = items.filter(
+        (i) => !i.system.advancement?.length
+      );
+      const multipleAdvancements =
+        items.length - itemsWithoutAdvancement.length > 1;
+      if (
+        multipleAdvancements &&
+        !game.settings.get('dnd5e', 'disableAdvancements')
+      ) {
+        ui.notifications.warn(
+          game.i18n.format('DND5E.WarnCantAddMultipleAdvancements')
+        );
+        items = itemsWithoutAdvancement;
+      }
+
+      // Filter out items already in containers to avoid creating duplicates
+      const containers = new Set(
+        items.filter((i) => i.type === 'container').map((i) => i._id)
+      );
+
+      items = items.filter((i) => !containers.has(i.system.container));
+
+      // Create the owned items & contents as normal
+      const toCreate = await dnd5e.documents.Item5e.createWithContents(items, {
+        transformFirst: (item: Item5e) =>
+          this._onDropSingleItem(item.toObject(), event),
+      });
+
+      const created = await dnd5e.documents.Item5e.createDocuments(toCreate, {
+        pack: this.actor.pack,
+        parent: this.actor,
+        keepId: true,
+      });
+
+      if (behavior === 'move') {
+        items.forEach((i) =>
+          fromUuid(i.uuid).then((d: Item5e) =>
+            d?.delete({ deleteContents: true })
+          )
+        );
+      }
+
+      return created;
+    }
+
+    /**
+     * Handles dropping of a single item onto this character sheet.
+     */
+    async _onDropSingleItem(
+      itemData: any,
+      event: DragEvent
+    ): Promise<object | boolean> {
+      const unsupportedItemTypes: Set<string> =
+        //@ts-expect-error
+        this.constructor.unsupportedItemTypes;
+
+      const isSupportedItemType = !unsupportedItemTypes.has(itemData.type);
+
+      // Check to make sure items of this type are allowed on this actor
+      if (!isSupportedItemType) {
+        ui.notifications.warn(
+          game.i18n.format('DND5E.ActorWarningInvalidItem', {
+            itemType: game.i18n.localize(CONFIG.Item.typeLabels[itemData.type]),
+            actorType: game.i18n.localize(
+              CONFIG.Actor.typeLabels[this.actor.type]
+            ),
+          })
+        );
+        return false;
+      }
+
+      const isOnInventoryTab = this.element.matches(
+        `:has([data-tab-id="${CONSTANTS.TAB_ACTOR_INVENTORY}"].active)`
+      );
+
+      // Create a Consumable spell scroll on the Inventory tab
+      if (
+        itemData.type === 'spell' &&
+        (isOnInventoryTab ||
+          this.actor.type === CONSTANTS.SHEET_TYPE_VEHICLE ||
+          this.actor.type === CONSTANTS.SHEET_TYPE_GROUP)
+      ) {
+        const options: Record<string, unknown> = {};
+
+        if (settings.value.includeFlagsInSpellScrollCreation) {
+          options.flags = itemData.flags;
+        }
+
+        const scroll = await dnd5e.documents.Item5e.createScrollFromSpell(
+          itemData,
+          options
+        );
+
+        return scroll?.toObject?.() ?? false;
+      }
+
+      // Clean up data
+      this._onDropResetData(itemData);
+
+      // Stack identical consumables
+      const stacked = this._onDropStackConsumables(itemData, {}, event);
+
+      if (stacked) {
+        return false;
+      }
+
+      // Bypass normal creation flow for any items with advancement
+      if (
+        this.actor.system.metadata?.supportsAdvancement &&
+        itemData.system.advancement?.length &&
+        !game.settings.get('dnd5e', 'disableAdvancements')
+      ) {
+        // Ensure that this item isn't violating the singleton rule
+        const dataModel =
+          CONFIG.Item.dataModels[
+            itemData.type as keyof typeof CONFIG.Item.dataModels
+          ];
+        const singleton = dataModel?.metadata.singleton ?? false;
+        if (singleton && this.actor.itemTypes[itemData.type].length) {
+          ui.notifications.error(
+            game.i18n.format('DND5E.ActorWarningSingleton', {
+              itemType: game.i18n.localize(
+                CONFIG.Item.typeLabels[itemData.type]
+              ),
+              actorType: game.i18n.localize(
+                CONFIG.Actor.typeLabels[this.actor.type]
+              ),
+            })
+          );
+          return false;
+        }
+
+        const manager =
+          dnd5e.applications.advacement.AdvancementManager.forNewItem(
+            this.actor,
+            itemData
+          );
+
+        if (manager.steps.length) {
+          manager.render(true);
+          return false;
+        }
+      }
+
+      // Adjust the preparation mode of a leveled spell depending on the section on which it is dropped.
+      if (itemData.type === 'spell') {
+        this._onDropSpell(itemData, event);
+      }
+
+      return itemData;
+    }
+
+    /**
+     * Stack identical consumables when a new one is dropped rather than creating a duplicate item.
+     */
+    _onDropStackConsumables(
+      itemData: any,
+      { container = null } = {},
+      event: DragEvent
+    ): Promise<Item5e> | null {
+      return FoundryAdapter.onDropStackConsumablesForActor(
+        this.actor,
+        itemData,
+        { container },
+        event
+      );
+    }
+
+    /**
+     * Reset certain pieces of data stored on items when they are dropped onto the actor.
+     */
+    _onDropResetData(itemData: any) {
+      if (!itemData.system) return;
+      ['attuned', 'equipped', 'prepared'].forEach(
+        (k) => delete itemData.system[k]
+      );
+    }
+
+    // TODO: Make this WORK! And update it to handle Always Prepared
+    /**
+     * Adjust the preparation mode of a dropped spell depending on the drop location on the sheet.
+     */
+    _onDropSpell(itemData: any, event: DragEvent) {
+      if (
+        ![CONSTANTS.SHEET_TYPE_NPC, CONSTANTS.SHEET_TYPE_CHARACTER].includes(
+          this.document.type
+        )
+      )
+        return;
+
+      const dropTarget = event?.target;
+
+      if (!dropTarget || !(dropTarget instanceof HTMLElement)) {
+        return;
+      }
+
+      // Determine the section it is dropped on, if any.
+      let header = dropTarget.closest<HTMLElement>('.items-header'); // Dropped directly on the header.
+      if (header === null) {
+        const list = dropTarget.closest<HTMLElement>('.item-list'); // Dropped inside an existing list.
+        header =
+          list?.previousElementSibling instanceof HTMLElement
+            ? list.previousElementSibling
+            : null;
+      }
+
+      const { level, preparationMode } =
+        header?.closest<HTMLElement>('[data-level]')?.dataset ?? {};
+
+      // Determine the actor's spell slot progressions, if any.
+      const spellcastKeys = Object.keys(CONFIG.DND5E.spellcastingTypes);
+      const progs = Object.values(this.document.classes).reduce<Set<string>>(
+        (acc: any, cls: any) => {
+          const type = cls.spellcasting?.type;
+          if (spellcastKeys.includes(type)) acc.add(type);
+          return acc;
+        },
+        new Set<string>()
+      );
+
+      const prep = itemData.system.preparation;
+
+      // Case 1: Drop a cantrip.
+      if (itemData.system.level === 0) {
+        const modes = CONFIG.DND5E.spellPreparationModes;
+
+        const mode =
+          modes[
+            preparationMode as keyof typeof CONFIG.DND5E.spellPreparationModes
+          ] ?? {};
+
+        if ('cantrips' in mode && mode.cantrips) {
+          prep.mode = 'prepared';
+        } else if (!preparationMode) {
+          const isCaster =
+            this.document.system.attributes.spell.level || progs.size;
+          prep.mode = isCaster ? 'prepared' : 'innate';
+        } else {
+          prep.mode = preparationMode;
+        }
+
+        if ('prepares' in mode && mode.prepares) {
+          prep.prepared = true;
+        }
+      }
+
+      // Case 2: Drop a leveled spell in a section without a mode.
+      else if (level === '0' || !preparationMode) {
+        if (this.document.type === 'npc') {
+          prep.mode = this.document.system.attributes.spell.level
+            ? 'prepared'
+            : 'innate';
+        } else {
+          const m = progs.has('leveled')
+            ? 'prepared'
+            : firstOfSet(progs) ?? 'innate';
+          prep.mode = progs.has(prep.mode) ? prep.mode : m;
+        }
+      }
+
+      // Case 3: Drop a leveled spell in a specific section.
+      else prep.mode = preparationMode;
+    }
+
+    async _onDropFolder(
+      event: DragEvent & { currentTarget: HTMLElement },
+      data: Record<string, any>
+    ): Promise<object | boolean | undefined> {
+      if (!this.actor.isOwner) {
+        return [];
+      }
+
+      const folder = await Folder.implementation.fromDropData(data);
+
+      if (folder.type !== 'Item') {
+        return [];
+      }
+
+      const droppedItemData = await Promise.all(
+        folder.contents.map(async (item: any) => {
+          if (!(item instanceof Item)) item = await fromUuid(item.uuid);
+          return item;
+        })
+      );
+
+      return this._onDropItemCreate(droppedItemData, event);
     }
   }
 
