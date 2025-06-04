e5073a2fc2671921669dbf05e7d3a5d59ad602c2
Kevin Garner <kevinleegarner@gmail.com>
2025-06-03 21:48:49 -0500

    Implemented roll and formula columns.

--- src/sheets/quadrone/item/columns/ItemDamageFormulasColumn.svelte
+++ src/sheets/quadrone/item/columns/ItemDamageFormulasColumn.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
   import { getSheetContext } from 'src/sheets/sheet-context.svelte';
   import type { ColumnCellProps } from 'src/runtime/item/item.types';
+  import Dnd5eIcon from 'src/components/icon/Dnd5eIcon.svelte';
 
-  let { rowDocument }: ColumnCellProps = $props();
+  let { rowDocument, rowContext }: ColumnCellProps = $props();
 
   let context = $derived(getSheetContext());
 </script>
 
-hello damages
+<div>
+  {#each rowDocument.labels.damages as damage}
+    {@const damageType = CONFIG.DND5E.damageTypes[damage.damageType]}
+    <div class="row">
+      <span class="formula">{damage.formula}</span>
+      <span data-tooltip aria-label={damage.label}>
+        <!-- 👋 hightouch - --icon-fill variable controls color -->
+        <Dnd5eIcon src={damageType.icon} />
+      </span>
+    </div>
+  {:else}
+    <span class="color-text-disabled">&mdash;</span>
+  {/each}
+</div>
--- src/sheets/quadrone/item/columns/ItemRollColumn.svelte
+++ src/sheets/quadrone/item/columns/ItemRollColumn.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
   import { getSheetContext } from 'src/sheets/sheet-context.svelte';
   import type { ColumnCellProps } from 'src/runtime/item/item.types';
+  import { getModifierData } from 'src/utils/formatting';
+  import { isNil } from 'src/utils/data';
 
-  let { rowDocument }: ColumnCellProps = $props();
+  let { rowDocument, rowContext }: ColumnCellProps = $props();
 
   let context = $derived(getSheetContext());
 </script>
 
-hello roll
+{#if !isNil(rowContext.toHit)}
+  {@const mod = getModifierData(rowContext.toHit)}
+  <span class="modifier">
+    <span class="sign">
+      {mod.sign}
+    </span>
+    <span class="value">
+      {mod.value}
+    </span>
+  </span>
+{:else if rowContext.save.ability}
+  <div class="stacked">
+    <span class="ability">{rowContext.save.ability}</span>
+    <span class="value">{rowContext.save.dc.value}</span>
+  </div>
+{:else}
+  <span class="color-text-disabled">&mdash;</span>
+{/if}
