f749a6086da8b80d5e23844991cf0b13fa19fd5c
kgar <kevinleegarner@gmail.com>
2025-07-02 22:47:02 -0500

    Updated wiki links.

--- src/constants.ts
+++ src/constants.ts
@@ -470,7 +470,7 @@ export const CONSTANTS = {
   },
   WIKI_LINK: 'https://github.com/kgar/foundry-vtt-tidy-5e-sheets/wiki',
   WIKI_LINK_CHANGE_ALL_SHEETS_GUIDE:
-    'https://github.com/kgar/foundry-vtt-tidy-5e-sheets/wiki/Using-Tidy5e#setting-tidy-as-your-default-sheet',
+    'https://github.com/kgar/foundry-vtt-tidy-5e-sheets/wiki/Using-Tidy5e#set-the-default-for-all-sheets',
   WIKI_LINK_CHANGE_SHEET_GUIDE:
-    'https://github.com/kgar/foundry-vtt-tidy-5e-sheets/wiki/Using-Tidy5e#changing-an-existing-sheet-to-tidy',
+    'https://github.com/kgar/foundry-vtt-tidy-5e-sheets/wiki/Using-Tidy5e#change-an-existing-sheet-to-tidy',
 } as const;
