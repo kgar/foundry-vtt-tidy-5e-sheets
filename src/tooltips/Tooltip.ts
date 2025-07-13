a9ed440786eee1cd484991263347af1fb9e01beb
kgar <kevinleegarner@gmail.com>
2025-07-13 18:36:46 -0500

    Added logic to enforce favorite items and activities to be owned by the actor.
    Restored Foundry core (or Tidy) min height to applications.

--- src/tooltips/Tooltip.ts
+++ src/tooltips/Tooltip.ts
@@ -2,7 +2,7 @@ export class Tooltip {
   static show(target: HTMLElement, markup: string, theme: string) {
     game.tooltip.activate(target, {
       html: markup,
-      cssClass: `tidy5e-sheet application quadrone themed theme-${theme}`,
+      cssClass: `tidy5e-sheet application quadrone tooltip themed theme-${theme}`,
     });
   }
 }
