aeb12b3ca163819da731f1d0a2eadd3a099347ae
Kevin Garner <kevinleegarner@gmail.com>
2025-08-04 14:16:13 -0500

    Upgraded Tidy Dungeons of Drakkenheim plugin to support Drakkenheim v1.7.1.

--- src/integration/modules/Drakkenheim/DrakkenheimCore.ts
+++ src/integration/modules/Drakkenheim/DrakkenheimCore.ts
@@ -2,6 +2,7 @@ import type { Tidy5eSheetsApi } from 'src/api/Tidy5eSheetsApi';
 import type { ModuleIntegrationBase } from 'src/integration/integration-classes';
 import DrakkenheimCoreContaminationTab from './DrakkenheimCoreContaminationTab.svelte';
 import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+import { DRAKKENHEIM_CORE_CONSTANTS } from './DrakkenheimCoreConstants';
 
 export class DrakkenheimCoreModuleIntegration implements ModuleIntegrationBase {
   get moduleId(): string {
@@ -11,13 +12,24 @@ export class DrakkenheimCoreModuleIntegration implements ModuleIntegrationBase {
   init(api: Tidy5eSheetsApi): void {
     import('./DrakkenheimContaminationTabClassic.scss');
 
+    // Since the setting requires a reload to toggle, we will simply avoid registering a column if it's disabled.
+    if (
+      FoundryAdapter.getGameSetting(
+        DRAKKENHEIM_CORE_CONSTANTS.MODULE_ID,
+        DRAKKENHEIM_CORE_CONSTANTS.SETTING_DISABLE_TAB
+      )
+    ) {
+      return;
+    }
+
     const contaminationTab = new api.models.SvelteTab({
-      title: () => FoundryAdapter.localize('DRAKKENHEIM.Contamination'),
+      title: () => FoundryAdapter.localize('DRAKKENHEIM.CONTAMINATION.tab'),
       tabId: 'drakkenheim-contamination-tab',
       component: DrakkenheimCoreContaminationTab,
       getContext(context) {
         return context;
       },
+      iconClass: 'fa-solid fa-meteor'
     });
 
     api.registerCharacterTab(contaminationTab, {
