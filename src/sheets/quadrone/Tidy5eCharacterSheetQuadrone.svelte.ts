70fff9ba1d24b5f5dec10a9525a3c7d4b09534c5
kgar <kevinleegarner@gmail.com>
2025-08-31 10:15:12 -0500

    Made inspiration sourcing logic static on the quadrone character sheet to support calling it from outside an instance of Character, allowing for Group sheet to do data prep properly.

--- src/sheets/quadrone/Tidy5eCharacterSheetQuadrone.svelte.ts
+++ src/sheets/quadrone/Tidy5eCharacterSheetQuadrone.svelte.ts
@@ -175,7 +175,7 @@ export class Tidy5eCharacterSheetQuadrone extends Tidy5eActorSheetQuadroneBase(
     };
 
     let inspirationSource: InspirationSource | undefined =
-      await this.tryGetInspirationSource();
+      await Tidy5eCharacterSheetQuadrone.tryGetInspirationSource(this.actor);
 
     let background = this.actor.system.details.background;
     let species = this.actor.system.details.race;
@@ -328,17 +328,16 @@ export class Tidy5eCharacterSheetQuadrone extends Tidy5eActorSheetQuadroneBase(
     return context;
   }
 
-  private async tryGetInspirationSource(): Promise<
-    InspirationSource | undefined
-  > {
+  public static async tryGetInspirationSource(
+    actor: Actor5e
+  ): Promise<InspirationSource | undefined> {
     let apiConfig = ActorInspirationRuntime.bankedInspirationConfig;
 
     if (!!apiConfig?.change && !!apiConfig?.getData) {
-      let data = await apiConfig.getData(this, this.actor);
+      let data = await apiConfig.getData(this, actor);
 
       return {
-        change: async (delta) =>
-          await apiConfig.change!(this, this.actor, delta),
+        change: async (delta) => await apiConfig.change!(this, actor, delta),
         value: data?.value ?? 0,
         max: data?.max ?? 0,
       };
@@ -348,8 +347,8 @@ export class Tidy5eCharacterSheetQuadrone extends Tidy5eActorSheetQuadroneBase(
       return;
     }
 
-    let inspirationSourceId = TidyFlags.inspirationSource.get(this.actor);
-    let inspirationSourceItem = this.actor.items.get(inspirationSourceId);
+    let inspirationSourceId = TidyFlags.inspirationSource.get(actor);
+    let inspirationSourceItem = actor.items.get(inspirationSourceId);
 
     let inspirationSource: InspirationSource | undefined;
 
@@ -426,8 +425,12 @@ export class Tidy5eCharacterSheetQuadrone extends Tidy5eActorSheetQuadroneBase(
           });
         continue;
       } else if (type === 'slots') {
-        const { value, max, level, type: method } =
-          this.actor.system.spells[id] ?? {};
+        const {
+          value,
+          max,
+          level,
+          type: method,
+        } = this.actor.system.spells[id] ?? {};
         const uses = { value, max, field: `system.spells.${id}.value` };
 
         const model = CONFIG.DND5E.spellcasting[method];
