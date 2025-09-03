7b7d6710066122f750b541b0d4d96ec826c4cae9
jeffwain <jeffwain@users.noreply.github.com>
2025-09-03 13:26:37 -0500

    Sidebar conflict resolved

--- src/sheets/quadrone/actor/group-parts/group-tab-sidebar/GroupTabSidebar.svelte
+++ src/sheets/quadrone/actor/group-parts/group-tab-sidebar/GroupTabSidebar.svelte
@@ -7,42 +7,30 @@
   import type { Ref } from 'src/features/reactivity/reactivity.types';
   import { CONSTANTS } from 'src/constants';
   import type { ClassValue } from 'svelte/elements';
+  import type { GroupMemberQuadroneContext } from 'src/types/types';
+  import GroupTraitPill from '../GroupTraitPill.svelte';
 
   let context = $derived(getGroupSheetQuadroneContext());
 
   const localize = FoundryAdapter.localize;
 
-  let emphasizedActorRef = getContext<Ref<string | undefined>>(
-    CONSTANTS.SVELTE_CONTEXT.EMPHASIZED_ACTOR_REF,
+  let emphasizedActorRef = getContext<
+    Ref<GroupMemberQuadroneContext | undefined>
+  >(CONSTANTS.SVELTE_CONTEXT.EMPHASIZED_MEMBER_REF);
+
+
+  let emphasizedMember = $derived(emphasizedActorRef.value);
+
+  let emphasizedActorUuid = $derived<string>(
+    emphasizedMember?.actor.uuid ?? '',
   );
-
-  let emphasizedActor = $derived(emphasizedActorRef.value);
-
-  let highlightColors = $derived.by(() => {
-    const colors = new Map<string, { highlightColor: string; backgroundColor: string }>();
-    
-    // Loop through all member types and populate highlight colors
-    const allMembers = [
-      ...context.members.character.members,
-      ...context.members.npc.members,
-      ...context.members.vehicle.members,
-    ];
-    
-    for (const member of allMembers) {
-      if (member.accentColor) {
-        const backgroundColor = `oklch(from ${member.accentColor} calc(l * 0.75) calc(c * 1.2) h)`;
-        const highlightColor = `oklch(from ${member.accentColor} calc(l * 1.4) 60% h)`;
-        
-        colors.set(member.actor.uuid, { highlightColor, backgroundColor });
-      }
-    }
-    
-    return colors;
-  });
-  
 </script>
 
-<aside class="sidebar flexcol" style:--t5e-member-color-hover={highlightColors.get(emphasizedActor ?? '')?.highlightColor} style:--t5e-member-color-background={highlightColors.get(emphasizedActor ?? '')?.backgroundColor}>
+<aside
+  class="sidebar"
+  style:--t5e-member-color-hover={`oklch(from ${emphasizedMember?.accentColor} calc(l * 1.4) 60% h)`}
+  style:--t5e-member-color-background={`oklch(from ${emphasizedMember?.accentColor} calc(l * 0.75) calc(c * 1.2) h)`}
+>
   <!-- Aggregate Traits -->
   <div class="list traits">
     <!-- Languages -->
@@ -57,42 +45,30 @@
         <div class="list-values">
           <ul class="pills">
             {#each context.traits.languages as language}
-              {@const isEmphasized = emphasizedActor !== undefined && language.identifiers.has(emphasizedActor)}
+              {@const isEmphasized =
+                emphasizedMember !== undefined &&
+                language.identifiers.has(emphasizedActorUuid)}
               {@const pillState: ClassValue = {
                 emphasized: isEmphasized,
                 'theme-dark': isEmphasized,
-                diminished: emphasizedActor !== undefined && !isEmphasized,
+                diminished: emphasizedMember !== undefined && !isEmphasized,
               }}
               {@const pill =
-                language.identifiers.get(emphasizedActor ?? '') ?? language}
+                language.identifiers.get(emphasizedActorUuid) ?? language}
 
-              <li
-                class={['pill pill-medium', pillState]}
-                data-tooltip-direction="UP"
-              >
-                <span class="label font-label-medium">{pill.label}</span>
-                {#if language.identifiers.size > 1}
-                  <span>
-                    {language.identifiers.size}
-                  </span>
-                {/if}
-                {#if pill.value && pill.units}
-                  <span>
-                    <span class="value font-data-medium">{pill.value}</span
-                    ><span class="units font-default-medium color-text-lighter"
-                      >{pill.units}</span
-                    >
-                  </span>
-                {:else if pill.value}
-                  <span class="value font-data-medium">{pill.value}</span>
-                {/if}
-              </li>
+              <GroupTraitPill
+                class={pillState}
+                count={language.identifiers.size}
+                label={pill.label}
+                units={pill.units}
+                value={pill.value}
+              />
             {/each}
           </ul>
         </div>
       </div>
     </div>
-    <!-- Speed -->
+    <!-- Speeds -->
     <div class="list-entry">
       <div class="list-label flexrow">
         <h4 class="font-weight-label">
@@ -104,30 +80,23 @@
         <div class="list-values">
           <ul class="pills">
             {#each context.traits.speeds as speed}
-              {@const isEmphasized = emphasizedActor !== undefined && speed.identifiers.has(emphasizedActor)}
+              {@const isEmphasized =
+                emphasizedMember !== undefined &&
+                speed.identifiers.has(emphasizedActorUuid)}
               {@const pillState: ClassValue = {
                 emphasized: isEmphasized,
                 'theme-dark': isEmphasized,
-                diminished: emphasizedActor !== undefined && !isEmphasized,
+                diminished: emphasizedMember !== undefined && !isEmphasized,
               }}
               {@const pill =
-                speed.identifiers.get(emphasizedActor ?? '') ?? speed}
-              <li
-                class={['pill pill-medium', pillState]}
-                data-tooltip-direction="UP"
-              >
-                <span class="label font-label-medium">{pill.label}</span>
-                {#if pill.value && pill.units}
-                  <span>
-                    <span class="value font-data-medium">{pill.value}</span
-                    ><span class="units font-default-medium color-text-lighter"
-                      >{pill.units}</span
-                    >
-                  </span>
-                {:else if pill.value}
-                  <span class="value font-data-medium">{pill.value}</span>
-                {/if}
-              </li>
+                speed.identifiers.get(emphasizedActorUuid) ?? speed}
+
+              <GroupTraitPill
+                class={pillState}
+                label={pill.label}
+                units={pill.units}
+                value={pill.value}
+              />
             {/each}
           </ul>
         </div>
@@ -145,42 +114,33 @@
         <div class="list-values">
           <ul class="pills">
             {#each context.traits.senses as sense}
-              {@const isEmphasized = emphasizedActor !== undefined && sense.identifiers.has(emphasizedActor)}
+              {@const isEmphasized =
+                emphasizedMember !== undefined &&
+                sense.identifiers.has(emphasizedActorUuid)}
               {@const pillState: ClassValue = {
                 emphasized: isEmphasized,
                 'theme-dark': isEmphasized,
-                diminished: emphasizedActor !== undefined && !isEmphasized,
+                diminished: emphasizedMember !== undefined && !isEmphasized,
               }}
               {@const pill =
-                sense.identifiers.get(emphasizedActor ?? '') ?? sense}
-              <li
-                class={['pill pill-medium', pillState]}
-                data-tooltip-direction="UP"
-              >
-                <span class="label font-label-medium">{pill.label}</span>
-                {#if pill.value && pill.units}
-                  <span>
-                    <span class="value font-data-medium">{pill.value}</span
-                    ><span class="units font-default-medium color-text-lighter"
-                      >{pill.units}</span
-                    >
-                  </span>
-                {:else if pill.value}
-                  <span class="value font-data-medium">{pill.value}</span>
-                {/if}
-              </li>
+                sense.identifiers.get(emphasizedActorUuid) ?? sense}
+
+              <GroupTraitPill
+                class={pillState}
+                label={pill.label}
+                units={pill.units}
+                value={pill.value}
+              />
             {/each}
           </ul>
         </div>
       </div>
     </div>
-  </div>
 
-  <!-- Aggregate Skills -->
-  <GroupSkills />
+    <!-- Aggregate Skills -->
+    <GroupSkills />
 
-  <!-- Aggregate Special -->
-  <div class="list traits">
+    <!-- Specials -->
     <div class="list-entry">
       <div class="list-label flexrow">
         <h4 class="font-weight-label">
@@ -192,25 +152,23 @@
         <div class="list-values">
           <ul class="pills">
             {#each context.traits.specials as special}
-              {@const isEmphasized = emphasizedActor !== undefined && special.identifiers.has(emphasizedActor)}
+              {@const isEmphasized =
+                emphasizedMember !== undefined &&
+                special.identifiers.has(emphasizedActorUuid)}
               {@const pillState: ClassValue = {
                 emphasized: isEmphasized,
                 'theme-dark': isEmphasized,
-                diminished: emphasizedActor !== undefined && !isEmphasized,
+                diminished: emphasizedMember !== undefined && !isEmphasized,
               }}
-              <li
-                class={['pill pill-medium', pillState]}
-                data-tooltip-direction="UP"
-              >
-                <span class="label font-label-medium">{special.label}</span>
-              </li>
+
+              <GroupTraitPill class={pillState} label={special.label} />
             {/each}
           </ul>
         </div>
       </div>
     </div>
 
-    <!-- Aggregate Tools -->
+    <!-- Tools -->
     <div class="list-entry">
       <div class="list-label flexrow">
         <h4 class="font-weight-label">
@@ -222,18 +180,15 @@
         <div class="list-values">
           <ul class="pills">
             {#each context.traits.tools as tool}
-              {@const isEmphasized = emphasizedActor !== undefined && tool.identifiers.has(emphasizedActor)}
+              {@const isEmphasized =
+                emphasizedMember !== undefined &&
+                tool.identifiers.has(emphasizedActorUuid)}
               {@const pillState: ClassValue = {
                 emphasized: isEmphasized,
                 'theme-dark': isEmphasized,
-                diminished: emphasizedActor !== undefined && !isEmphasized,
+                diminished: emphasizedMember !== undefined && !isEmphasized,
               }}
-              <li
-                class={['pill pill-medium', pillState]}
-                data-tooltip-direction="UP"
-              >
-                <span class="label font-label-medium">{tool.label}</span>
-              </li>
+              <GroupTraitPill class={pillState} label={tool.label} />
             {/each}
           </ul>
         </div>
