83b27b70a9d0bf0b5ecec85a4fa61b40b1caf082
jeffwain <jeffwain@users.noreply.github.com>
2025-09-03 13:19:19 -0500

    More merge conflicts resolved

---
+++ src/sheets/quadrone/item/columns/GroupVehicleDtColumn.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { ColumnCellProps } from 'src/runtime/types';
+  import type { GroupMemberQuadroneContext } from 'src/types/types';
+
+  let {
+    rowDocument,
+    rowContext,
+  }: ColumnCellProps<any, GroupMemberQuadroneContext> = $props();
+</script>
+
+<span
+  class="font-data-large color-text-{rowDocument.system.attributes.hp.dt
+    ? 'default'
+    : 'lightest'}">{rowDocument.system.attributes.hp.dt ?? '—'}</span
+>
