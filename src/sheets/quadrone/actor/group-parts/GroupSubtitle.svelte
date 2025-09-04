6f3c9e5452426ab8cfca16a72010ec3b001fab64
kgar <kevinleegarner@gmail.com>
2025-09-04 14:55:13 -0500

    Removed redundant award button.

--- src/sheets/quadrone/actor/group-parts/GroupSubtitle.svelte
+++ src/sheets/quadrone/actor/group-parts/GroupSubtitle.svelte
@@ -123,20 +123,6 @@
       {/if}
     </span>
   {/if}
-  {#if context.unlocked}
-    <button
-      aria-label={awardAriaLabel}
-      type="button"
-      class={[
-        'button button-borderless button-icon-only button-config flexshrink',
-      ]}
-      data-tooltip
-      onclick={() => context.sheet.award()}
-      data-tidy-sheet-part="ability-configuration-control"
-    >
-      <i class="fas fa-trophy"></i>
-    </button>
-  {/if}
 </div>
 
 <div
