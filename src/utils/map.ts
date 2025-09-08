a72f8558ff45894b31456ad307a95d1d5d5ba232
kgar <kevinleegarner@gmail.com>
2025-09-07 23:49:25 -0500

    Added some JsDoc to remind myself about replacing the helper when getOrInsert is widely available.

--- src/utils/map.ts
+++ src/utils/map.ts
@@ -1,3 +1,7 @@
+/** 
+ * Gets an entry from a map. If the entry doesn't exist, the value is inserted and then returned. 
+ * This helper is temporary and is meant to be replaced by Map.prototype.getOrInsert. @see https://github.com/tc39/proposal-upsert
+ */
 export function mapGetOrInsert<K, V>(
   map: Map<K, V>,
   key: K,
@@ -9,6 +13,10 @@ export function mapGetOrInsert<K, V>(
   return map.get(key) as V;
 }
 
+/** 
+ * Gets an entry from a map. If the entry doesn't exist, the value is inserted via a callback function and then returned. 
+ * This helper is temporary and is meant to be replaced by Map.prototype.getOrInsert. @see https://github.com/tc39/proposal-upsert
+ */
 export function getOrInsertComputed<K, V>(
   map: Map<K, V>,
   key: K,
