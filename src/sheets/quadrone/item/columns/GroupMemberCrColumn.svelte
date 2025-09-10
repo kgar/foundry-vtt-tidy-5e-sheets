79dd102dbe2fac1a49a1703665360d6121dddcd2
jeffwain <jeffwain@users.noreply.github.com>
2025-09-09 20:37:07 -0500

    CR column

---
+++ src/sheets/quadrone/item/columns/GroupMemberCrColumn.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { ColumnCellProps } from 'src/runtime/types';
+  import type { EncounterMemberQuadroneContext } from 'src/types/types';
+
+  let {
+    rowDocument,
+    rowContext,
+  }: ColumnCellProps<any, EncounterMemberQuadroneContext> = $props();
+</script>
+
+<span class="font-label-large color-text-default"
+  >{rowDocument.system.attributes.cr.value}</span
+>
