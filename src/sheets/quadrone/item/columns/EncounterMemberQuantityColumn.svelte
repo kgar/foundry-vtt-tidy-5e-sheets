32ddc8e58d91f88e99f45503daddbfc8de764cf6
kgar <kevinleegarner@gmail.com>
2025-09-11 22:10:07 -0500

    Wired up quantity tracker for encounter member quantity column.

--- src/sheets/quadrone/item/columns/EncounterMemberQuantityColumn.svelte
+++ src/sheets/quadrone/item/columns/EncounterMemberQuantityColumn.svelte
@@ -1,20 +1,38 @@
 <script lang="ts">
   import type { ColumnCellProps } from 'src/runtime/types';
   import type { EncounterMemberQuadroneContext } from 'src/types/types';
-  import TextInputQuadrone from 'src/components/inputs/TextInputQuadrone.svelte';
+  import { getEncounterSheetQuadroneContext } from 'src/sheets/sheet-context.svelte';
+  import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+
+  import InlineQuantityTracker from 'src/components/trackers/InlineQuantityTracker.svelte';
+  import { tick } from 'svelte';
 
   let {
     rowDocument,
     rowContext,
   }: ColumnCellProps<any, EncounterMemberQuadroneContext> = $props();
+
+  let context = $derived(getEncounterSheetQuadroneContext());
+
+  let localize = FoundryAdapter.localize;
+
+  let quantity = $derived(rowContext.quantity.value);
 </script>
 
-<!-- {#if unlocked} -->
-<!-- <TextInputQuadrone
-  class="uninput"
-  tooltip="Quantity roll formula"
-  /> -->
-<!-- {/if} -->
-<span class="font-label-large color-text-default"
-  >{rowContext.quantity.value}</span
->
+{#if context.unlocked}
+  <InlineQuantityTracker
+    aria-label={localize('DND5E.Quantity')}
+    data-tooltip="DND5E.Quantity"
+    min="0"
+    value={quantity}
+    onchange={async (ev) =>
+      await context.sheet.updateMemberQuantity(
+        rowDocument.uuid,
+        ev.currentTarget.value,
+      )}
+  />
+{:else}
+  <span class="font-label-large color-text-default"
+    >{rowContext.quantity.value}</span
+  >
+{/if}
