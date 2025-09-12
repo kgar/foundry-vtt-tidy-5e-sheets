75fd1b484a7d4fdbc56d9233c3b8224b2033c5a3
kgar <kevinleegarner@gmail.com>
2025-09-11 22:35:51 -0500

    Converted inline item quantity tracker to reuse the generic inline tracker and build atop it.

--- src/components/trackers/InlineItemQuantityTracker.svelte
+++ src/components/trackers/InlineItemQuantityTracker.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import type { Item5e } from 'src/types/item.types';
-  import TextInput from '../inputs/TextInput.svelte';
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
-  import { isNil } from 'src/utils/data';
+  import InlineQuantityTracker from './InlineQuantityTracker.svelte';
+  import { processInputChangeDeltaFromValues } from 'src/utils/form';
 
   interface Props {
     item: Item5e;
@@ -12,43 +12,29 @@
 
   let { item, field = 'system.quantity', disabled = false }: Props = $props();
 
-  let quantity = $derived(FoundryAdapter.getProperty<number>(item, field));
+  let quantity = $derived(
+    FoundryAdapter.getProperty<number>(item, field)?.toString() ?? '',
+  );
 
-  async function increment() {
-    if (!isNil(quantity)) {
-      await item.update({
-        [field]: quantity + 1,
-      });
-    }
-  }
-
-  async function decrement() {
-    if (!isNil(quantity)) {
-      await item.update({
-        [field]: quantity - 1,
-      });
-    }
-  }
+  let localize = FoundryAdapter.localize;
 </script>
 
-<article class="tidy-inline-item-quantity-tracker" class:disabled>
-  <a class="command decrementer" onclick={() => !disabled && decrement()}>
-    <i class="fa-solid fa-minus"></i>
-  </a>
-  <span class="item-quantity-tracker-input-wrapper uninput">
-    <TextInput
-      class="item-quantity-tracker-input"
-      document={item}
-      {field}
-      value={quantity}
-      {disabled}
-      allowDeltaChanges={true}
-      selectOnFocus={true}
-    ></TextInput>
-  </span>
-  <a class="command incrementer" onclick={() => !disabled && increment()}>
-    <i class="fa-solid fa-plus"></i>
-  </a>
-</article>
+<InlineQuantityTracker
+  aria-label={localize('DND5E.Quantity')}
+  data-tooltip="DND5E.Quantity"
+  min="0"
+  {disabled}
+  onchange={async (ev) => {
+    const input = ev.currentTarget;
 
-<!-- TODO: Make a generic counter that can be injected onto the sheet for things like `So Inspired!` 🔥 -->
+    await item.update({
+      [field]: processInputChangeDeltaFromValues(
+        ev.currentTarget.value,
+        quantity,
+      ),
+    });
+
+    input.value = quantity;
+  }}
+  value={quantity}
+/>
