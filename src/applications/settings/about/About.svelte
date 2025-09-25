f5f08d0a72fc7ffec716f63a36efcc7cc515c653
kgar <kevinleegarner@gmail.com>
2025-09-24 22:45:19 -0500

    Updated the Tidy icon/logo on the About component.

--- src/applications/settings/about/About.svelte
+++ src/applications/settings/about/About.svelte
@@ -4,66 +4,68 @@
   const localize = FoundryAdapter.localize;
 </script>
 
-<div class="tidy-info-banner">
-  <a href="https://foundryvtt.com/packages/tidy5e-sheet/" target="_blank">
-    <img
-      class="logo"
-      src="../modules/tidy5e-sheet/images/tidy-5e-logo.svg"
-      alt={localize('TIDY5E.Settings.About.logoAltText')}
-    />
-  </a>
-  <p class="banner-text">
-    {@html localize('TIDY5E.Settings.About.MaintainerParagraph', {
-      urlStart: '<a href="https://github.com/kgar" target="_blank">',
+<div class="dialog-content-container flexcol">
+  <div class="tidy-info-banner">
+    <a href="https://foundryvtt.com/packages/tidy5e-sheet/" target="_blank">
+      <img
+        class="logo"
+        src="../modules/tidy5e-sheet/images/tidy-shield-large.webp"
+        alt={localize('TIDY5E.Settings.About.logoAltText')}
+      />
+    </a>
+    <p class="banner-text">
+      {@html localize('TIDY5E.Settings.About.MaintainerParagraph', {
+        urlStart: '<a href="https://github.com/kgar" target="_blank">',
+        urlEnd: '</a>',
+      })}
+    </p>
+  </div>
+  <p>
+    {@html localize('TIDY5E.Settings.About.GithubParagraph', {
+      urlStart:
+        '<a href="https://github.com/kgar/foundry-vtt-tidy-5e-sheets/issues" target="_blank">',
       urlEnd: '</a>',
     })}
   </p>
+  
+  <h3>{localize('TIDY5E.Settings.About.FinancialSupportHeader')}</h3>
+  <p>
+    {@html localize('TIDY5E.Settings.About.DiscordParagraph', {
+      urlStart: '<a href="https://discord.gg/kdqbcWJrYU" target="_blank">',
+      urlEnd: '</a>',
+    })}
+  </p>
+  <p>
+    {@html localize('TIDY5E.Settings.About.LocalizationParagraph', {
+      urlStart:
+        '<a href="https://hosted.weblate.org/projects/foundry-vtt-tidy-5e-sheets/" target="_blank">',
+      urlEnd: '</a>',
+    })}
+  </p>
+  <p>
+    {localize('TIDY5E.Settings.About.FinancialSupportParagraph')}
+  </p>
+  <ul>
+    <li>
+      {@html localize('TIDY5E.Settings.About.BuyMeACoffeeLink', {
+        urlStart: '<a href="https://www.buymeacoffee.com/kgar" target="_blank">',
+        urlEnd: '</a>',
+      })}
+    </li>
+    <li>
+      {@html localize('TIDY5E.Settings.About.KoFiLink', {
+        urlStart: '<a href="https://ko-fi.com/iamkgar" target="_blank">',
+        urlEnd: '</a>',
+      })}
+    </li>
+    <li>
+      {@html localize('TIDY5E.Settings.About.PatreonLink', {
+        urlStart: '<a href="https://www.patreon.com/iamkgar" target="_blank">',
+        urlEnd: '</a>',
+      })}
+    </li>
+  </ul>
 </div>
-<p>
-  {@html localize('TIDY5E.Settings.About.GithubParagraph', {
-    urlStart:
-      '<a href="https://github.com/kgar/foundry-vtt-tidy-5e-sheets/issues" target="_blank">',
-    urlEnd: '</a>',
-  })}
-</p>
-
-<h3>{localize('TIDY5E.Settings.About.FinancialSupportHeader')}</h3>
-<p>
-  {@html localize('TIDY5E.Settings.About.DiscordParagraph', {
-    urlStart: '<a href="https://discord.gg/kdqbcWJrYU" target="_blank">',
-    urlEnd: '</a>',
-  })}
-</p>
-<p>
-  {@html localize('TIDY5E.Settings.About.LocalizationParagraph', {
-    urlStart:
-      '<a href="https://hosted.weblate.org/projects/foundry-vtt-tidy-5e-sheets/" target="_blank">',
-    urlEnd: '</a>',
-  })}
-</p>
-<p>
-  {localize('TIDY5E.Settings.About.FinancialSupportParagraph')}
-</p>
-<ul>
-  <li>
-    {@html localize('TIDY5E.Settings.About.BuyMeACoffeeLink', {
-      urlStart: '<a href="https://www.buymeacoffee.com/kgar" target="_blank">',
-      urlEnd: '</a>',
-    })}
-  </li>
-  <li>
-    {@html localize('TIDY5E.Settings.About.KoFiLink', {
-      urlStart: '<a href="https://ko-fi.com/iamkgar" target="_blank">',
-      urlEnd: '</a>',
-    })}
-  </li>
-  <li>
-    {@html localize('TIDY5E.Settings.About.PatreonLink', {
-      urlStart: '<a href="https://www.patreon.com/iamkgar" target="_blank">',
-      urlEnd: '</a>',
-    })}
-  </li>
-</ul>
 
 <style lang="scss">
   .tidy-info-banner {
