fa0dd50a0175570fe4997b602b8362effac54b8b
kgar <kevinleegarner@gmail.com>
2025-09-24 21:21:10 -0500

    Added dialog class wrapper to homebrew settings

--- src/applications/homebrew-settings/HomebrewSettings.svelte
+++ src/applications/homebrew-settings/HomebrewSettings.svelte
@@ -17,51 +17,53 @@
   const idPrefix = foundry.utils.randomID();
 </script>
 
-<div class="flexcol flex1">
-  <fieldset>
-    <legend>
-      <h2>{localize('TIDY5E.Settings.BankedInspiration.title')}</h2>
-      <tidy-gold-header-underline></tidy-gold-header-underline>
-    </legend>
-
-    <div class="form-group slim">
-      <label for="banked-inspiration-{idPrefix}">
-        {localize('TIDY5E.Settings.EnableBankedInspiration.name')}
-      </label>
-      <div class="form-fields">
-        <input
-          id="banked-inspiration-{idPrefix}"
-          type="checkbox"
-          bind:checked={config.enableBankedInspiration}
-        />
+<div class="dialog-content-container flexcol">
+  <div class="flexcol flex1">
+    <fieldset>
+      <legend>
+        <h2>{localize('TIDY5E.Settings.BankedInspiration.title')}</h2>
+        <tidy-gold-header-underline></tidy-gold-header-underline>
+      </legend>
+  
+      <div class="form-group slim">
+        <label for="banked-inspiration-{idPrefix}">
+          {localize('TIDY5E.Settings.EnableBankedInspiration.name')}
+        </label>
+        <div class="form-fields">
+          <input
+            id="banked-inspiration-{idPrefix}"
+            type="checkbox"
+            bind:checked={config.enableBankedInspiration}
+          />
+        </div>
+        <p class="hint">
+          {localize('TIDY5E.Settings.EnableBankedInspiration.hint')}
+        </p>
       </div>
-      <p class="hint">
-        {localize('TIDY5E.Settings.EnableBankedInspiration.hint')}
-      </p>
-    </div>
-    <div class="form-group slim">
-      <label for="banked-inspiration-gm-only-{idPrefix}">
-        {localize('TIDY5E.Settings.BankedInspirationGmOnly.name')}
-      </label>
-      <div class="form-fields">
-        <input
-          id="banked-inspiration-gm-only-{idPrefix}"
-          type="checkbox"
-          bind:checked={config.bankedInspirationGmOnly}
-        />
+      <div class="form-group slim">
+        <label for="banked-inspiration-gm-only-{idPrefix}">
+          {localize('TIDY5E.Settings.BankedInspirationGmOnly.name')}
+        </label>
+        <div class="form-fields">
+          <input
+            id="banked-inspiration-gm-only-{idPrefix}"
+            type="checkbox"
+            bind:checked={config.bankedInspirationGmOnly}
+          />
+        </div>
+        <p class="hint">
+          {localize('TIDY5E.Settings.BankedInspirationGmOnly.hint')}
+        </p>
       </div>
-      <p class="hint">
-        {localize('TIDY5E.Settings.BankedInspirationGmOnly.hint')}
-      </p>
-    </div>
-  </fieldset>
-</div>
-<div class="flexrow flex0">
-  <button
-    type="button"
-    class="button button-primary save-changes-btn"
-    onclick={() => app.save()}
-  >
-    {localize('TIDY5E.SaveChanges')}
-  </button>
+    </fieldset>
+  </div>
+  <div class="flexrow flex0">
+    <button
+      type="button"
+      class="button button-primary save-changes-btn"
+      onclick={() => app.save()}
+    >
+      {localize('TIDY5E.SaveChanges')}
+    </button>
+  </div>
 </div>
