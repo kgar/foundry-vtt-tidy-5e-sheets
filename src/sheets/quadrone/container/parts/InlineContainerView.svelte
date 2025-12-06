1dd95373ea946475c138e0ef0d2b728ea3f8c134
jeffwain <jeffwain@users.noreply.github.com>
2025-12-06 11:55:51 -0600

    Updated inline containers to display currencies and allow transferring to parent sheet. Removed unused filigree in containers.

--- src/sheets/quadrone/container/parts/InlineContainerView.svelte
+++ src/sheets/quadrone/container/parts/InlineContainerView.svelte
@@ -9,6 +9,7 @@
   import { getSearchResultsContext } from 'src/features/search/search.svelte';
   import type { MessageBus } from 'src/types/types';
   import { Tidy5eContainerSheetQuadrone } from '../../Tidy5eContainerSheetQuadrone.svelte';
+  import TextInputQuadrone from 'src/components/inputs/TextInputQuadrone.svelte';
   import { SheetSections } from 'src/features/sections/SheetSections';
   import { UserSheetPreferencesService } from 'src/features/user-preferences/SheetPreferencesService';
   import { TidyFlags } from 'src/api';
@@ -30,6 +31,8 @@
     searchCriteria,
     sheetDocument,
   }: Props = $props();
+
+  let currencies = $derived(containerContents.currencies);
 
   let toggleServiceMap = $derived(inlineToggleService.map);
 
@@ -80,6 +83,38 @@
       TidyFlags.sectionConfig.get(container)?.[tabId],
     ),
   );
+
+  // Check if container has any currency to transfer
+  let hasCurrency = $derived(
+    currencies.some((c) => c.value > 0),
+  );
+
+  // Transfer all currency from container to parent actor
+  async function transferCurrencyToParent() {
+    if (!sheetDocument || !hasCurrency) return;
+
+    const currencyKeys = Object.keys(CONFIG.DND5E.currencies);
+
+    // Build update objects for both documents
+    const containerUpdate: Record<string, number> = {};
+    const actorUpdate: Record<string, number> = {};
+
+    for (const key of currencyKeys) {
+      const containerValue = container.system.currency[key] ?? 0;
+      const actorValue = sheetDocument.system.currency[key] ?? 0;
+
+      if (containerValue > 0) {
+        containerUpdate[`system.currency.${key}`] = 0;
+        actorUpdate[`system.currency.${key}`] = actorValue + containerValue;
+      }
+    }
+
+    // Update both documents
+    await Promise.all([
+      container.update(containerUpdate),
+      sheetDocument.update(actorUpdate),
+    ]);
+  }
 </script>
 
 <ExpandableContainer
@@ -87,7 +122,8 @@
   class={!searchResults.show(container.uuid) ? 'hidden' : ''}
   deferRendering
 >
-  <div class="inline-content-view filigree-guideline-and-contents full-height">
+
+  <div class="inline-content-view full-height">
     <div
       class="flex-column extra-small-gap flex-1 inline-container-view"
       data-tidy-container-id={container.id}
@@ -108,6 +144,45 @@
             >{FoundryAdapter.localize('TIDY5E.EmptyContainer')}</span
           >
         </div>
+      {/if}
+    </div>
+
+    <div class="currency-container flexrow flex1 extra-small-gap align-items-center">
+      {#each currencies as currency (currency.key)}
+        <label class="input-group">
+          <i class="currency {currency.key}" aria-label={currency.key}></i>
+          <TextInputQuadrone
+            document={container}
+            field="system.currency.{currency.key}"
+            id="{container.id}-system.currency.{currency.key}"
+            value={currency.value}
+            enableDeltaChanges={true}
+            selectOnFocus={true}
+            disabled={!editable}
+            class="currency-item uninput currency-{currency.key}"
+            placeholder="0"
+          />
+          <span class="denomination {currency.key}" data-denom={currency.key}>
+            {currency.abbr}
+          </span>
+        </label>
+      {/each}
+      {#if editable && hasCurrency}
+        <a
+          role="button"
+          tabindex="0"
+          class="button button-secondary transfer-currency flexshrink"
+          onclick={transferCurrencyToParent}
+          title={FoundryAdapter.localize(
+            'DND5E.CurrencyManager.Transfer.Label',
+          )}
+          data-tooltip={FoundryAdapter.localize(
+            'TIDY5E.Containers.TransferCurrencyToParent.Tooltip',
+          )}
+        >
+          <i class="fas fa-person-arrow-up-from-line"></i>
+          {FoundryAdapter.localize('DND5E.CurrencyManager.Transfer.Label')}
+          </a>
       {/if}
     </div>
   </div>
