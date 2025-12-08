053abdae7bb8ca6205b2195f994c6294935f2b8c
jeffwain <jeffwain@users.noreply.github.com>
2025-12-08 00:20:37 -0600

    Fixed concentration row display in saving throws card. Now correctly shows up only if the sheet has spellcasting.

--- src/sheets/quadrone/actor/character-parts/SavingThrowsCard.svelte
+++ src/sheets/quadrone/actor/character-parts/SavingThrowsCard.svelte
@@ -57,7 +57,7 @@
     {/each}
     <!-- </ul> -->
     <!-- Concentration here -->
-    {#if context.saves.concentration}
+    {#if 'spellcasting' in context && context.spellcasting?.length > 0 && context.saves.concentration}
       {@const conc = context.saves.concentration}
       {@const tooltip = localize('DND5E.AbilityConfigure', {
         ability: context.saves.concentration.label,
