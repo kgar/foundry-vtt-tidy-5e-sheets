89c7b42738ca494e76782503c2300441da2cd3b2
jeffwain <jeffwain@users.noreply.github.com>
2026-01-24 12:44:29 -0600

    Fixed broken label, added some todos.

--- src/runtime/item/default-item-filters.ts
+++ src/runtime/item/default-item-filters.ts
@@ -65,7 +65,7 @@ export const defaultItemFilters: Record<string, ItemFilter> = {
       !!Activities.getVisibleActivities(item, item.system.activities)?.some(
         (a: any) => a.activation?.type === CONSTANTS.ACTIVATION_COST_CREW
       ),
-    text: 'DND5E.VehicleCrewAction',
+    text: 'DND5E.ACTIVATION.Type.Crew.Label',
   },
   [CONSTANTS.FILTER_ACTIVATION_COST_SPECIAL]: {
     name: 'activationCostSpecial',
@@ -167,12 +167,12 @@ export function getItemRarityFilters(): ItemFilter[] {
 
   return Object.entries(itemRarity).map<ItemFilter>(
     ([key, text]) =>
-      ({
-        name: key,
-        predicate: (item) =>
-          !FoundryAdapter.concealDetails(item) && item.system.rarity === key,
-        text: text,
-      } satisfies ItemFilter)
+    ({
+      name: key,
+      predicate: (item) =>
+        !FoundryAdapter.concealDetails(item) && item.system.rarity === key,
+      text: text,
+    } satisfies ItemFilter)
   );
 }
 
@@ -206,11 +206,11 @@ export function getSpellSchoolFilters(): ItemFilter[] {
 
   return Object.entries(spellSchools).map<ItemFilter>(
     ([key, schoolData]) =>
-      ({
-        name: key,
-        predicate: (item) => item.system.school === key,
-        text: schoolData.label,
-      } satisfies ItemFilter)
+    ({
+      name: key,
+      predicate: (item) => item.system.school === key,
+      text: schoolData.label,
+    } satisfies ItemFilter)
   );
 }
 
@@ -245,7 +245,7 @@ export function getAttunementFilters(): ItemFilter[] {
       predicate: (item) =>
         !FoundryAdapter.concealDetails(item) &&
         !!CONFIG.DND5E.attunementTypes[
-          item.system.attunement as keyof typeof CONFIG.DND5E.attunementTypes
+        item.system.attunement as keyof typeof CONFIG.DND5E.attunementTypes
         ] &&
         item.system.attuned,
       text: 'DND5E.AttunementAttuned',
