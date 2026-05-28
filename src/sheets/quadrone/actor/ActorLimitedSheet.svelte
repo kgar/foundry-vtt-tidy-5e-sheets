8c61a8a20a183575cf97f2feec1b24d35ba8fc63
kgar <kevinleegarner@gmail.com>
2026-05-28 08:25:20 -0500

    Corrected limited actor sheet styles.
    Added support for Details section for groups and encounters and Biography section for PCs/NPCs/Vehicles.

--- src/sheets/quadrone/actor/ActorLimitedSheet.svelte
+++ src/sheets/quadrone/actor/ActorLimitedSheet.svelte
@@ -5,9 +5,12 @@
   import ActorPortrait from './parts/ActorPortrait.svelte';
   import type {
     CharacterSheetQuadroneContext,
+    EncounterSheetQuadroneContext,
+    GroupSheetQuadroneContext,
     NpcSheetQuadroneContext,
     VehicleSheetQuadroneContext,
   } from 'src/types/types';
+  import { coalesce } from 'src/utils/formatting';
 
   let context =
     $derived(
@@ -15,10 +18,28 @@
         | CharacterSheetQuadroneContext
         | NpcSheetQuadroneContext
         | VehicleSheetQuadroneContext
+        | EncounterSheetQuadroneContext
+        | GroupSheetQuadroneContext
       >(),
     );
 
   const localize = FoundryAdapter.localize;
+
+  const biography = $derived(
+    context.type === CONSTANTS.SHEET_TYPE_NPC
+      ? coalesce(context.enriched.publicBiography, '&nbsp;')
+      : context.type === CONSTANTS.SHEET_TYPE_CHARACTER ||
+          context.type === CONSTANTS.SHEET_TYPE_VEHICLE
+        ? coalesce(context.enriched.biography, '&nbsp;')
+        : null,
+  );
+
+  const description = $derived(
+    context.type == CONSTANTS.SHEET_TYPE_GROUP ||
+      context.type === CONSTANTS.SHEET_TYPE_ENCOUNTER
+      ? coalesce(context.enriched.description.full, '&nbsp;')
+      : null,
+  );
 </script>
 
 <header class="sheet-header limited-sheet flexcol theme-dark">
@@ -30,17 +51,24 @@
   </div>
 </header>
 <div class="sheet-body limited-sheet scroll-container">
-  <div class="editor-rendered-content">
-    <h3>
-      {localize('DND5E.Biography')}
-    </h3>
-    <tidy-gold-header-underline></tidy-gold-header-underline>
-    {#if context.type === CONSTANTS.SHEET_TYPE_NPC}
-      {@html context.enriched.publicBiography}
-    {:else}
-      {@html context.enriched.biography}
-    {/if}
-  </div>
+  {#if description}
+    <div class="editor-rendered-content">
+      <h3>
+        {localize('DND5E.Description')}
+      </h3>
+      <tidy-gold-header-underline></tidy-gold-header-underline>
+      {@html description}
+    </div>
+  {/if}
+  {#if biography}
+    <div class="editor-rendered-content">
+      <h3>
+        {localize('DND5E.Biography')}
+      </h3>
+      <tidy-gold-header-underline></tidy-gold-header-underline>
+      {@html biography}
+    </div>
+  {/if}
   {#if context.type === CONSTANTS.SHEET_TYPE_NPC && context.enriched.appearance}
     <div class="editor-rendered-content">
       <h3>
