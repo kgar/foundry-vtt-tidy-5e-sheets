6bf74ac1f25f96e982b0d2a845876a4302d411e8
jeffwain <jeffwain@users.noreply.github.com>
2026-06-15 22:16:52 -0500

    Fixed Drakkenheim tab config resolution

--- src/integration/modules/Drakkenheim/SebastianCrowesGuideToDrakkenheim.ts
+++ src/integration/modules/Drakkenheim/SebastianCrowesGuideToDrakkenheim.ts
@@ -1,8 +1,7 @@
 import type { Tidy5eSheetsApi } from 'src/api/Tidy5eSheetsApi';
 import type { ModuleIntegrationBase } from 'src/integration/integration-classes';
-import DrakkenheimCoreContaminationTab from './DrakkenheimCoreContaminationTab.svelte';
-import { FoundryAdapter } from 'src/foundry/foundry-adapter';
 import { DRAKKENHEIM_CORE_CONSTANTS } from './DrakkenheimCoreConstants';
+import { registerDrakkenheimContaminationTab } from './drakkenheim-functions';
 
 export class SebastianCrowesGuideToDrakkenheimModuleIntegration
   implements ModuleIntegrationBase
@@ -12,40 +11,6 @@ export class SebastianCrowesGuideToDrakkenheimModuleIntegration
   }
 
   init(api: Tidy5eSheetsApi): void {
-    import('./DrakkenheimContaminationTabClassic.less');
-    import('./DrakkenheimContaminationTab.css');
-    
-    // Since the setting requires a reload to toggle, we will simply avoid registering a column if it's disabled.
-    if (
-      FoundryAdapter.getGameSetting<boolean>(
-        DRAKKENHEIM_CORE_CONSTANTS.SEBASTIAN_CROWE_MODULE_ID,
-        DRAKKENHEIM_CORE_CONSTANTS.SETTING_DISABLE_TAB
-      )
-    ) {
-      return;
-    }
-
-    const contaminationTab = new api.models.SvelteTab({
-      title: () => FoundryAdapter.localize('DRAKKENHEIM.CONTAMINATION.tab'),
-      tabId: 'drakkenheim-contamination-tab',
-      component: DrakkenheimCoreContaminationTab,
-      getContext(context: Map<string, any>) {
-        context ??= new Map<string, any>();
-        context.set(
-          DRAKKENHEIM_CORE_CONSTANTS.SVELTE_CONTEXT.VERSION,
-          FoundryAdapter.getGameSetting(
-            DRAKKENHEIM_CORE_CONSTANTS.SEBASTIAN_CROWE_MODULE_ID,
-            DRAKKENHEIM_CORE_CONSTANTS.SETTING_VERSION
-          )
-        );
-        return context;
-      },
-      iconClass: 'fa-solid fa-meteor',
-    });
-
-    api.registerCharacterTab(contaminationTab, {
-      layout: ['classic', 'quadrone'],
-    });
-    api.registerNpcTab(contaminationTab, { layout: ['classic', 'quadrone'] });
+    registerDrakkenheimContaminationTab(api, this.moduleId);
   }
 }
---
+++ src/integration/modules/Drakkenheim/drakkenheim-functions.ts
@@ -0,0 +1,74 @@
+import type { Tidy5eSheetsApi } from 'src/api/Tidy5eSheetsApi';
+import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+import DrakkenheimCoreContaminationTab from './DrakkenheimCoreContaminationTab.svelte';
+import { DRAKKENHEIM_CORE_CONSTANTS } from './DrakkenheimCoreConstants';
+
+function getDrakkenheimModuleScope(): string | undefined {
+  for (const moduleId of DRAKKENHEIM_CORE_CONSTANTS.PRIORITIZED_SCOPE_MODULE_IDS) {
+    // Gets the first active module in the priority order
+    if (game.modules.get(moduleId)?.active) {
+      return moduleId;
+    }
+  }
+
+  return undefined;
+}
+
+function getOptionalGameSetting<T>(
+  namespace: string,
+  settingName: string
+): T | undefined {
+  if (!FoundryAdapter.hasGameSetting(namespace, settingName)) {
+    return undefined;
+  }
+
+  return FoundryAdapter.getGameSetting<T>(namespace, settingName);
+}
+
+export function registerDrakkenheimContaminationTab(
+  api: Tidy5eSheetsApi,
+  moduleId: string
+): void {
+  const scope = getDrakkenheimModuleScope();
+
+  // Drakkenheim registers settings on a single module based on the getScope() priority order
+  if (!scope || moduleId !== scope) {
+    return;
+  }
+
+  import('./DrakkenheimContaminationTabClassic.less');
+  import('./DrakkenheimContaminationTab.css');
+
+  // Since the setting requires a reload to toggle, we will simply avoid registering a column if it's disabled.
+  if (
+    getOptionalGameSetting<boolean>(
+      scope,
+      DRAKKENHEIM_CORE_CONSTANTS.SETTING_DISABLE_TAB
+    )
+  ) {
+    return;
+  }
+
+  const contaminationTab = new api.models.SvelteTab({
+    title: () => FoundryAdapter.localize('DRAKKENHEIM.CONTAMINATION.tab'),
+    tabId: 'drakkenheim-contamination-tab',
+    component: DrakkenheimCoreContaminationTab,
+    getContext(context: Map<string, any>) {
+      context ??= new Map<string, any>();
+      context.set(
+        DRAKKENHEIM_CORE_CONSTANTS.SVELTE_CONTEXT.VERSION,
+        getOptionalGameSetting<string>(
+          scope,
+          DRAKKENHEIM_CORE_CONSTANTS.SETTING_VERSION
+        ) ?? DRAKKENHEIM_CORE_CONSTANTS.VERSION_MODERN
+      );
+      return context;
+    },
+    iconClass: 'fa-solid fa-meteor',
+  });
+
+  api.registerCharacterTab(contaminationTab, {
+    layout: ['classic', 'quadrone'],
+  });
+  api.registerNpcTab(contaminationTab, { layout: ['classic', 'quadrone'] });
+}
