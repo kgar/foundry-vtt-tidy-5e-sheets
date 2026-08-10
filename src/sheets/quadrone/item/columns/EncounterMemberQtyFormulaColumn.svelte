e455ec525c9c2274ff12e04f1efd1048eccebee8
Kevin Garner <kevinleegarner@gmail.com>
2026-08-10 15:51:26 -0500

    - [x] src\sheets\quadrone\item\columns\EncounterMemberQtyFormulaColumn.svelte - data-name

--- src/sheets/quadrone/item/columns/EncounterMemberQtyFormulaColumn.svelte
+++ src/sheets/quadrone/item/columns/EncounterMemberQtyFormulaColumn.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
   import { InputAttachments } from 'src/attachments/input-attachments.svelte';
   import { getEncounterSheetQuadroneContext } from 'src/sheets/sheet-context.svelte';
-  import type {
-    EncounterMemberCombatantQuadroneContext,
-    EncounterMemberQuadroneContext,
-  } from 'src/types/types';
+  import type { EncounterMemberQuadroneContext } from 'src/types/types';
 
   let {
     rowContext,
@@ -13,20 +10,11 @@
   } = $props();
 
   let context = $derived(getEncounterSheetQuadroneContext());
-
-  async function handleChange(
-    ev: Event & { currentTarget: EventTarget & HTMLInputElement },
-    memberContext: EncounterMemberQuadroneContext,
-  ) {
-    const input = ev.currentTarget;
-    const newValue = input.value ?? '';
-    await context.sheet.updateMemberFormula(memberContext.actor.uuid, newValue);
-  }
 </script>
 
 <input
   type="text"
-  onchange={(ev) => handleChange(ev, rowContext)}
+  data-name="quantity.formula"
   {@attach InputAttachments.selectOnFocus}
   value={rowContext.quantity.formula}
   placeholder={context.unlocked ? '1d4+1' : '—'}
