f81b2702de0843453a7039625201f9c0eafd7156
jeffwain <jeffwain@users.noreply.github.com>
2026-08-16 10:39:36 -0500

    Actions bar

--- src/sheets/quadrone/actor/tabs/GroupBastionsTab.svelte
+++ src/sheets/quadrone/actor/tabs/GroupBastionsTab.svelte
@@ -1,32 +1,41 @@
-<script>
-    import ButtonMenu from "src/components/button-menu/ButtonMenu.svelte";
+<script lang="ts">
+  import { observeResize } from 'src/features/resize-observation/attachments';
+  import { FoundryAdapter } from 'src/foundry/foundry-adapter';
+  import { getGroupSheetQuadroneContext } from 'src/sheets/sheet-context.svelte';
+  import { CONSTANTS } from 'src/constants';
+  import { createSearchResultsState } from 'src/features/search/search.svelte';
+  import BastionFacilitiesTable from '../group-parts/bastions/BastionFacilitiesTable.svelte';
+  import BastionOrdersTable from '../group-parts/bastions/BastionOrdersTable.svelte';
+  import GroupBastionsActionBar from '../../shared/GroupBastionsActionBar.svelte';
 
+  let context = $derived(getGroupSheetQuadroneContext());
+
+  const localize = FoundryAdapter.localize;
+
+  let searchCriteria = $state('');
+  let searchResults = createSearchResultsState();
+
+  let sectionsInlineWidth: number = $state(0);
+
+  function onResize(entry: ResizeObserverEntry) {
+    sectionsInlineWidth = entry.borderBoxSize[0].inlineSize;
+  }
+
+  let hasMembers = $derived(context.bastionsContext.members.length > 0);
 </script>
-<aside class="sidebar expanded">
-  Sidebar likely holds group bastion content.
-</aside>
-<div class="tab-content">
-  <div class="tab-actions button-bar">
-    <button
-      class="button button-secondary"
-      type="button"
-      onclick={() => {
-        console.log('Take a bastion turn');
-      }}
-    >
-      <i class="fa-solid fa-poo-storm"></i>
-      Take a Bastion Turn
-    </button>
-    <button
-    class="button button-secondary"
-    type="button"
-    onclick={() => {
-      console.log('Take a bastion turn');
-    }}
-  >
-    <i class="fa-solid fa-poo-storm"></i>
-    Roll Bastion Event (Maintain)
-  </button>
+
+<div class="tab-right-column">
+  <GroupBastionsActionBar bind:searchCriteria tabId={CONSTANTS.TAB_GROUP_BASTIONS} />
+
+  <div class="tab-content" {@attach observeResize(onResize)}>
+    {#if hasMembers}
+      <!-- <BastionOrdersTable {sectionsInlineWidth} />
+
+      <BastionFacilitiesTable {sectionsInlineWidth} /> -->
+    {:else}
+      <div class="empty-state-container empty-state-description">
+        {localize('TIDY5E.Bastion.Group.EmptyStateHint')}
+      </div>
+    {/if}
   </div>
-  Table has individual player bastion content.
 </div>
