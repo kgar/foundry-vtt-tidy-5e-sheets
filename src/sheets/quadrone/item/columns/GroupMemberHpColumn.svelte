e4253fd2f2b3421201ccf935edf49537958c9568
jeffwain <jeffwain@users.noreply.github.com>
2026-09-24 23:08:01 -0500

    Forgot a button

--- src/sheets/quadrone/item/columns/GroupMemberHpColumn.svelte
+++ src/sheets/quadrone/item/columns/GroupMemberHpColumn.svelte
@@ -8,7 +8,6 @@
   import { getGroupSheetQuadroneContext } from 'src/sheets/sheet-context.svelte';
   import { FoundryAdapter } from 'src/foundry/foundry-adapter';
   import { CONSTANTS } from 'src/constants';
-  import Button from 'src/components/buttons/Button.svelte';
   import { getContext } from 'svelte';
   import type GroupMemberHpTooltip from 'src/tooltips/GroupMemberHpTooltip.svelte';
 
@@ -63,15 +62,16 @@
   </div>
 </div>
 {#if 'canEdit' in rowContext && rowContext.canEdit}
-  <Button
-    variant="borderless"
-    iconOnly
-    icon="fa-solid fa-dice-d20"
+  <button
+    type="button"
+    class="button button-borderless button-icon-only"
     aria-label={localize('DND5E.HPFormulaRollMessage')}
-    data-tooltip=""
+    data-tooltip
     onclick={async () => {
       await new Tidy5eNpcSheetQuadrone({ document: rowDocument }).rollFormula();
       context.sheet.render();
     }}
-  />
+  >
+    <i class="fa-solid fa-dice-d20"></i>
+  </button>
 {/if}
