148219c7be6a721eb09ffda01fea2500b9a514f3
jeffwain <jeffwain@users.noreply.github.com>
2026-09-24 23:09:44 -0500

    New image preloading
    This one I'm not sure if it's going to be worth it. This keeps our images in memory so that you don't get a flash of unstyled content whenever a sheet reloads. Doesn't include any item images or portraits, or even the item sheet sidebar background.

--- src/utils/preload-images.ts
+++ src/utils/preload-images.ts
@@ -1,16 +1,23 @@
 import { PRELOADED_IMAGE_PATHS } from './preloaded-images.generated';
 
+// Foundry uses `Cache-Control: no-cache` for static images. Hopefully
+// this keeps them in memory, but this might use too much memory with
+// the header banners. TBD.
+const preloadedImages: HTMLImageElement[] = [];
+
 // Run through the manifest and preload all images
 export function preloadSheetImages() {
   const run = () => {
     for (const imagePath of PRELOADED_IMAGE_PATHS) {
       const image = new Image();
       image.src = foundry.utils.getRoute(imagePath);
+      image.decode().catch(() => {});
+      preloadedImages.push(image);
     }
   };
 
   if (typeof requestIdleCallback === 'function') {
-    requestIdleCallback(run);
+    requestIdleCallback(run, { timeout: 2000 });
   } else {
     window.setTimeout(run, 0);
   }
